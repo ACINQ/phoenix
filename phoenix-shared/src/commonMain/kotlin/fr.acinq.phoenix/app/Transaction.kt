5ed5ee11534b0b623b85acd70cbc37e3d801dd53
Salomon BRYS <salomon@kodein.net>
2020-08-24 14:53:03 +0200

    Transaction status

--- phoenix-shared/src/commonMain/kotlin/fr.acinq.phoenix/app/Transaction.kt
+++ phoenix-shared/src/commonMain/kotlin/fr.acinq.phoenix/app/Transaction.kt
@@ -3,11 +3,15 @@ package fr.acinq.phoenix.app
 data class Transaction(
     val amountSat: Long,
     val desc: String, // Swift does not support fields named description
-    val success: Boolean,
+    val status: Status,
     val paymentHash: String,
     val paymentRequest: String,
     val paymentPreimage: String,
     val creationTimestamp: Long,
     val expirationTimestamp: Long,
     val completionTimestamp: Long
-)
+) {
+
+    enum class Status { Success, Pending, Failure }
+
+}
