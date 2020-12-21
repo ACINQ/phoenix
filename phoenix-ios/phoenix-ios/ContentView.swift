ACINQ/phoenix

commit 3c92cc2eceb63187917ad5fcaa641da6ad7d5fb9

Bug fix: biometrics prompt not appearing on app launch. Plus incorrect image showing for faceID devices (#84)

phoenix-ios/phoenix-ios/views/LockView.swift
@@ -8,6 +8,8 @@ struct LockView : View {
 	@State var isFaceID = false
 	@State var errorMsg: String? = nil
 	
+	@State var biometricsAttemptInProgress = false
+	
 	let willEnterForegroundPublisher = NotificationCenter.default.publisher(for:
 		UIApplication.willEnterForegroundNotification
 	)
@@ -65,31 +67,20 @@ struct LockView : View {
 		.background(Color(UIColor.systemBackground))
 		.edgesIgnoringSafeArea(.all)
 		.onAppear {
-			onAppear()
+			checkStatusAndMaybePrompt()
 		}
 		.onReceive(willEnterForegroundPublisher, perform: { _ in
-			onWillEnterForeground()
+			checkStatusAndMaybePrompt()
 		})
 	}
 	
-	func onAppear() -> Void {
+	func checkStatusAndMaybePrompt() -> Void {
 		
 		// This function is called when:
 		// - app is launched for the first time
-		// - app has just been backgrounded
-		
-		let status = AppSecurity.shared.biometricStatus()
-		updateBiometricsStatus(status)
-		
-		if (status != .notAvailable) && (UIApplication.shared.applicationState == .active) {
-			tryBiometricsLogin()
-		}
-	}
-	
-	func onWillEnterForeground() -> Void {
-		print("onWillEnterForeground()")
-		
-		// When the app returns from being in the background, the biometric status may have changed.
+		// - app returns from being in the background
+		//
+		// Note that after returning background, the biometric status may have changed.
 		// For example: .touchID_notEnrolled => .touchID_available
 		
 		let status = AppSecurity.shared.biometricStatus()
@@ -107,6 +98,12 @@ struct LockView : View {
 			case .touchID_notEnrolled  : fallthrough
 			case .touchID_notAvailable : isTouchID = true
 			default                    : isTouchID = false
+		}
+		switch status {
+			case .faceID_available    : fallthrough
+			case .faceID_notEnrolled  : fallthrough
+			case .faceID_notAvailable : isFaceID = true
+			default                   : isFaceID = false
 		}
 		
 		switch status {
@@ -130,7 +127,14 @@ struct LockView : View {
 	
 	func tryBiometricsLogin() -> Void {
 		
+		if biometricsAttemptInProgress {
+			return
+		}
+		biometricsAttemptInProgress = true
+		
 		AppSecurity.shared.tryUnlockWithBiometrics {(result: Result<[String], Error>) in
+			
+			biometricsAttemptInProgress = false
 			
 			switch result {
 				case .success(let mnemonics):

commit cfd43c88d517fea2c5924316d1e0dd9fd72c24c3

Bug fix: touchID prompt was appearing when backgrounding the app. This bug was introduced in commit 3c92cc2

phoenix-ios/phoenix-ios.xcodeproj/project.pbxproj
@@ -48,6 +48,7 @@
 		DC67654E25655D93004D4263 /* Colors.xcassets in Resources */ = {isa = PBXBuildFile; fileRef = DC67654D25655D93004D4263 /* Colors.xcassets */; };
 		DC682FE8258175CE00CA1114 /* Popover.swift in Sources */ = {isa = PBXBuildFile; fileRef = DC682FE7258175CE00CA1114 /* Popover.swift */; };
 		DC8107D3255F1FC1002084A3 /* RecoverySeedView.swift in Sources */ = {isa = PBXBuildFile; fileRef = DC8107D2255F1FC1002084A3 /* RecoverySeedView.swift */; };
+		DC89857F25914747007B253F /* UIApplicationState+Phoenix.swift in Sources */ = {isa = PBXBuildFile; fileRef = DC89857E25914747007B253F /* UIApplicationState+Phoenix.swift */; };
 		DCACF6F02566D0A60009B01E /* Data+Hexadecimal.swift in Sources */ = {isa = PBXBuildFile; fileRef = DCACF6EF2566D0A60009B01E /* Data+Hexadecimal.swift */; };
 		DCACF6FA2566D0BA0009B01E /* KeyStoreError.swift in Sources */ = {isa = PBXBuildFile; fileRef = DCACF6F52566D0BA0009B01E /* KeyStoreError.swift */; };
 		DCACF6FB2566D0BA0009B01E /* GenericPasswordStore.swift in Sources */ = {isa = PBXBuildFile; fileRef = DCACF6F62566D0BA0009B01E /* GenericPasswordStore.swift */; };
@@ -148,6 +149,7 @@
 		DC67654D25655D93004D4263 /* Colors.xcassets */ = {isa = PBXFileReference; lastKnownFileType = folder.assetcatalog; path = Colors.xcassets; sourceTree = "<group>"; };
 		DC682FE7258175CE00CA1114 /* Popover.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = Popover.swift; sourceTree = "<group>"; };
 		DC8107D2255F1FC1002084A3 /* RecoverySeedView.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = RecoverySeedView.swift; sourceTree = "<group>"; };
+		DC89857E25914747007B253F /* UIApplicationState+Phoenix.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = "UIApplicationState+Phoenix.swift"; sourceTree = "<group>"; };
 		DCACF6EF2566D0A60009B01E /* Data+Hexadecimal.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = "Data+Hexadecimal.swift"; sourceTree = "<group>"; };
 		DCACF6F52566D0BA0009B01E /* KeyStoreError.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = KeyStoreError.swift; sourceTree = "<group>"; };
 		DCACF6F62566D0BA0009B01E /* GenericPasswordStore.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = GenericPasswordStore.swift; sourceTree = "<group>"; };
@@ -380,6 +382,7 @@
 			isa = PBXGroup;
 			children = (
 				DCACF6EF2566D0A60009B01E /* Data+Hexadecimal.swift */,
+				DC89857E25914747007B253F /* UIApplicationState+Phoenix.swift */,
 			);
 			path = extensions;
 			sourceTree = "<group>";
@@ -641,6 +644,7 @@
 				53BEF96EC93BBB4B6C9E0845 /* lang.swift in Sources */,
 				53BEF4D5663CD782DC63DF63 /* colors.swift in Sources */,
 				C8D7ABC95B979B59AF5A7CA7 /* InitializationView.swift in Sources */,
+				DC89857F25914747007B253F /* UIApplicationState+Phoenix.swift in Sources */,
 				C8D7A84CCF914B08BDB03BE6 /* RestoreWalletView.swift in Sources */,
 				53BEFFEBEA9EBE7B27B53AF4 /* TransactionView.swift in Sources */,
 				53BEF1748D4A541C0BEEDF2B /* models.swift in Sources */,

phoenix-ios/phoenix-ios/ContentView.swift
@@ -1,5 +1,15 @@
 import SwiftUI
 import PhoenixShared
+import os.log
+
+#if DEBUG && false
+fileprivate var log = Logger(
+	subsystem: Bundle.main.bundleIdentifier!,
+	category: "ContentView"
+)
+#else
+fileprivate var log = Logger(OSLog.disabled)
+#endif
 
 struct ContentView: View {
 
@@ -113,7 +123,7 @@ struct ContentView: View {
 
 
 	private func onAppLaunch() -> Void {
-		print("onAppLaunch()")
+		log.trace("onAppLaunch()")
 
 		AppSecurity.shared.tryUnlockWithKeychain {(mnemonics: [String]?, enabledSecurity: EnabledSecurity) in
 
@@ -134,7 +144,7 @@ struct ContentView: View {
 	}
 
 	private func onDidEnterBackground() -> Void {
-		print("onDidEnterBackground()")
+		log.trace("onDidEnterBackground()")
 
 		let currentSecurity = AppSecurity.shared.enabledSecurity.value
 		enabledSecurity = currentSecurity

phoenix-ios/phoenix-ios/extensions/UIApplicationState+Phoenix.swift
@@ -1,0 +1,14 @@
+import UIKit
+
+
+extension UIApplication.State: CustomStringConvertible {
+	
+	public var description: String {
+		switch self {
+			case .inactive   : return "inactive"
+			case .active     : return "active"
+			case .background : return "background"
+			default          : return "unknown"
+		}
+	}
+}

phoenix-ios/phoenix-ios/views/LockView.swift
@@ -1,4 +1,14 @@
 import SwiftUI
+import os.log
+
+#if DEBUG && false
+fileprivate var log = Logger(
+	subsystem: Bundle.main.bundleIdentifier!,
+	category: "LockView"
+)
+#else
+fileprivate var log = Logger(OSLog.disabled)
+#endif
 
 struct LockView : View {
 	
@@ -67,14 +77,40 @@ struct LockView : View {
 		.background(Color(UIColor.systemBackground))
 		.edgesIgnoringSafeArea(.all)
 		.onAppear {
-			checkStatusAndMaybePrompt()
+			onAppear()
 		}
 		.onReceive(willEnterForegroundPublisher, perform: { _ in
-			checkStatusAndMaybePrompt()
+			willEnterForeground()
 		})
 	}
 	
-	func checkStatusAndMaybePrompt() -> Void {
+	func onAppear() -> Void {
+		log.trace("onAppear()")
+		
+		// This function may be called when:
+		//
+		// - The application has just finished launching.
+		//   In this case: applicationState == .active
+		//
+		// - The user is backgrounding the app, so the ContentView is switching in the LockView for security.
+		//   In this case: applicationState == .background
+		
+		log.debug("UIApplication.shared.applicationState = \(UIApplication.shared.applicationState)")
+		let canPrompt = UIApplication.shared.applicationState != .background
+		
+		checkStatus(canPrompt: canPrompt)
+	}
+	
+	func willEnterForeground() -> Void {
+		log.trace("willEnterForeground()")
+		
+		// NB: At this moment in time: UIApplication.shared.applicationState == .background
+		
+		checkStatus(canPrompt: true)
+	}
+	
+	func checkStatus(canPrompt: Bool) -> Void {
+		log.trace("checkStatusAndMaybePrompt()")
 		
 		// This function is called when:
 		// - app is launched for the first time
@@ -86,7 +122,7 @@ struct LockView : View {
 		let status = AppSecurity.shared.biometricStatus()
 		updateBiometricsStatus(status)
 		
-		if status != .notAvailable {
+		if (status != .notAvailable) && canPrompt {
 			tryBiometricsLogin()
 		}
 	}
@@ -126,8 +162,10 @@ struct LockView : View {
 	}
 	
 	func tryBiometricsLogin() -> Void {
+		log.trace("tryBiometricsLogin()")
 		
 		if biometricsAttemptInProgress {
+			log.debug("tryBiometricsLogin(): ignoring - already in progress")
 			return
 		}
 		biometricsAttemptInProgress = true
@@ -143,7 +181,7 @@ struct LockView : View {
 						isUnlocked = true
 					}
 				case .failure(let error):
-					print("error: \(error)")
+					log.debug("tryUnlockWithBiometrics: error: \(String(describing: error))")
 			}
 		}
 	}