3c281b05864a86d7b5540cd711f3469fd7a62605
Robbie Hanson <robbiehanson@deusty.com>
2021-01-26 13:03:20 -0500

    Bumping build number, and deleting old channels database.

--- phoenix-ios/phoenix-ios/ContentView.swift
+++ phoenix-ios/phoenix-ios/ContentView.swift
@@ -154,21 +154,33 @@ struct ContentView: View {
 	private func performVersionUpgradeChecks() -> Void {
 		
 		// Upgrade check(s)
-		//
+		
+		let key = "lastVersionCheck"
+		let previousBuild = UserDefaults.standard.string(forKey: key) ?? "3"
+		
 		// v0.7.3 (build 4)
 		// - serialization change for Channels
 		// - attempting to deserialize old version causes crash
 		// - we decided to delete old channels database (due to low number of test users)
-		
-		let key = "lastVersionCheck"
-		let str = UserDefaults.standard.string(forKey: key) ?? "3"
-		
-		if str.isVersion(lessThan: "4") {
+		//
+		if previousBuild.isVersion(lessThan: "4") {
 		
 			migrateChannelsDbFiles()
+		}
+		
+		// v0.7.3 (build 5)
+		// - serialization change for Channels
+		// - attempting to deserialize old version causes crash
+		//
+		if previousBuild.isVersion(lessThan: "5") {
 			
-			let version = Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? "4"
-			UserDefaults.standard.set(version, forKey: key)
+			migrateChannelsDbFiles()
+		}
+		
+		let currentBuild = Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? "0"
+		if previousBuild.isVersion(lessThan: currentBuild) {
+
+			UserDefaults.standard.set(currentBuild, forKey: key)
 		}
 	}
 	
