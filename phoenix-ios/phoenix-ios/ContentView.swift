f826364d62f1059f474006d2583f3885fbb3e8a3
Robbie Hanson <robbiehanson@deusty.com>
2021-04-16 12:14:49 -0500

    Bug fix for #145 - TransactionView is visible when Phoenix should be locked

--- phoenix-ios/phoenix-ios/ContentView.swift
+++ phoenix-ios/phoenix-ios/ContentView.swift
@@ -22,70 +22,43 @@ struct GlobalEnvironment: ViewModifier {
 
 struct ContentView: MVIView {
 
-    static func UIKitAppearance() {
-        let appearance = UINavigationBarAppearance()
-        appearance.configureWithOpaqueBackground()
-        
-        UINavigationBar.appearance().scrollEdgeAppearance = appearance
-        UINavigationBar.appearance().compactAppearance = appearance
-        UINavigationBar.appearance().standardAppearance = appearance
-    }
-
 	@StateObject var mvi = MVIState({ $0.content() })
 	
 	@Environment(\.controllerFactory) var factoryEnv
 	var factory: ControllerFactory { return factoryEnv }
 
-	@State private var isUnlocked = false
-	@State private var unlockedOnce = false
-	@State private var enabledSecurity = EnabledSecurity()
+	@ObservedObject var lockState: LockState
+	@State var unlockedOnce = false
 	
 	@Environment(\.popoverState) private var popoverState: PopoverState
 	@State private var popoverItem: PopoverItem? = nil
-
-	let didEnterBackgroundPublisher = NotificationCenter.default.publisher(for:
-		UIApplication.didEnterBackgroundNotification
-	)
 	
 	@ViewBuilder
 	var view: some View {
 
 		ZStack {
 			
-			if isUnlocked || !enabledSecurity.isEmpty {
-
-				if isUnlocked || unlockedOnce {
-					primaryView()
-						.zIndex(0) // needed for proper animation
-						.onAppear {
-							unlockedOnce = true
-						}
-
-					if let popoverItem = popoverItem {
-						PopoverWrapper(dismissable: popoverItem.dismissable) {
-							popoverItem.view
-						}
-						.zIndex(1) // needed for proper animation
+			if lockState.isUnlocked || unlockedOnce {
+			
+				primaryView()
+					.zIndex(0) // needed for proper animation
+					.onAppear {
+						unlockedOnce = true
 					}
-				}
 
-				if !isUnlocked {
-					LockView(isUnlocked: $isUnlocked)
-						.zIndex(2) // needed for proper animation
-						.transition(.asymmetric(
-							insertion : .identity,
-							removal   : .move(edge: .bottom)
-						))
+				if let popoverItem = popoverItem {
+					PopoverWrapper(dismissable: popoverItem.dismissable) {
+						popoverItem.view
+					}
+					.zIndex(1) // needed for proper animation
 				}
-				
-			} else { // App Launch
+			
+			} else { // prior to first unlock
 
 				NavigationView {
-					loadingView().onAppear {
-						onAppLaunch()
-					}
+					loadingView()
 				}
-				.zIndex(3) // needed for proper animation
+				.zIndex(2) // needed for proper animation
 				.transition(.asymmetric(
 					insertion : .identity,
 					removal   : .opacity
@@ -94,9 +67,6 @@ struct ContentView: MVIView {
 			
 		} // </ZStack>
 		.modifier(GlobalEnvironment())
-		.onReceive(didEnterBackgroundPublisher, perform: { _ in
-			onDidEnterBackground()
-		})
 		.onReceive(popoverState.display) { (newPopoverItem: PopoverItem) in
 			withAnimation {
 				popoverItem = newPopoverItem
@@ -135,128 +105,26 @@ struct ContentView: MVIView {
 		.navigationBarTitle("", displayMode: .inline)
 		.navigationBarHidden(true)
 	}
-
-
-	private func onAppLaunch() -> Void {
-		log.trace("onAppLaunch()")
-		
-		AppSecurity.shared.tryUnlockWithKeychain {(mnemonics: [String]?, enabledSecurity: EnabledSecurity) in
-
-			// There are multiple potential configurations:
-			//
-			// - no security       => mnemonics are available, enabledSecurity is empty
-			// - standard security => mnemonics are available, enabledSecurity is non-empty
-			// - advanced security => mnemonics are not available, enabledSecurity is non-empty
-			//
-			// Another way to think about it:
-			// - standard security => touchID only protects the UI, wallet can immediately be loaded
-			// - advanced security => touchID required to unlock both the UI and the seed
-
-			if let mnemonics = mnemonics {
-				// unlock & load wallet
-				AppDelegate.get().loadWallet(mnemonics: mnemonics)
-			}
-
-			self.isUnlocked = enabledSecurity.isEmpty
-			self.enabledSecurity = enabledSecurity
-		}
-	}
-
-	private func onDidEnterBackground() -> Void {
-		log.trace("onDidEnterBackground()")
-
-		let currentSecurity = AppSecurity.shared.enabledSecurity.value
-		enabledSecurity = currentSecurity
-		//
-		// Bug alert :
-		//   Even though it *looks* like we just updated the `enabledSecurity` value,
-		//   if we read it right now, we'll still see the old value !!!
-		//
-		// 	 For example, we might see something crazy like this:
-		//
-		//   print("currentSecurity: \(currentSecurity)") => 1
-		//   print("enabledSecurity: \(enabledSecurity)") => 0
-		//
-		if !currentSecurity.isEmpty {
-			self.isUnlocked = false
-		}
-	}
-}
-
-struct PardonOurMess: View {
-	
-	@Environment(\.popoverState) private var popoverState: PopoverState
-	@Environment(\.openURL) var openURL
-	
-	var body: some View {
-		
-		// Pardon our mess
-		// We refactored our database code to support versioning.
-		// The bad news: your existing channels have been forced closed.
-		// The good news: this shouldn't ever happen again.
-		//
-		// Help | OK
-		
-		VStack(alignment: HorizontalAlignment.leading) {
-			
-			HStack {
-				Spacer()
-				Image(systemName: "wrench.and.screwdriver.fill")
-					.imageScale(.medium)
-					.padding(.trailing, 4)
-				Text("Pardon our mess")
-					.font(.title2)
-				Spacer()
-			}
-			.padding(.bottom, 10)
-			
-			Text("Your app has been updated and your channels have been removed. Thanks for testing.")
-				.padding(.bottom, 20)
-			
-			HStack {
-				Button {
-					helpButtonTapped()
-				} label: {
-					Text("Help").font(.title3)
-				}
-				Spacer()
-				Button {
-					okButtonTapped()
-				} label: {
-					Text("OK").font(.title3)
-				}
-			}
-		}
-		.padding()
-	}
-	
-	func helpButtonTapped() {
-		log.trace("(PardonOurMess) helpButtonTapped()")
-		
-		let str = "https://phoenix.acinq.co/faq#my-channels-got-force-closed-how-do-i-recover-my-money"
-		if let url = URL(string: str) {
-			openURL(url)
-		}
-	}
-	
-	func okButtonTapped() {
-		log.trace("(PardonOurMess) okButtonTapped()")
-		popoverState.close.send()
-	}
 }
 
 
 class ContentView_Previews: PreviewProvider {
 
+	static var lockState_true = LockState(isUnlocked: true)
+	static var lockState_false = LockState(isUnlocked: false)
+	
 	static var previews: some View {
 		
-		ContentView().mock(Content.ModelWaiting())
+		ContentView(lockState: lockState_true)
+			.mock(Content.ModelWaiting())
 			.previewDevice("iPhone 11")
 		
-//		ContentView().mock(Content.ModelNeedInitialization())
+//		ContentView(lockState: lockState_true)
+//			.mock(Content.ModelNeedInitialization())
 //			.previewDevice("iPhone 11")
-//		
-//		ContentView().mock(Content.ModelIsInitialized())
+		
+//		ContentView(lockState: lockState_false)
+//			.mock(Content.ModelIsInitialized())
 //			.previewDevice("iPhone 11")
 	}
 }
--- phoenix-ios/phoenix-ios/SceneDelegate.swift
+++ phoenix-ios/phoenix-ios/SceneDelegate.swift
@@ -12,11 +12,24 @@ fileprivate var log = Logger(
 fileprivate var log = Logger(OSLog.disabled)
 #endif
 
+class LockState: ObservableObject {
+	@Published var isUnlocked: Bool
+
+	init(isUnlocked: Bool) {
+		self.isUnlocked = isUnlocked
+	}
+}
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 	var window: UIWindow?
 	var cancellables = Set<AnyCancellable>()
+	
+	var lockWindow: UIWindow?
+	
+	let lockState = LockState(isUnlocked: false)
+	
+	var isAppLaunch = true
 
 	func scene(
 		_ scene: UIScene,
@@ -25,12 +38,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 	) {
 		log.trace("scene(_:willConnectTo:options:)")
 		
-		let contentView = ContentView()
-		
 		if let windowScene = scene as? UIWindowScene {
 			
+			let contentView = ContentView(lockState: lockState)
+			
 			let window = UIWindow(windowScene: windowScene)
-		   window.rootViewController = UIHostingController(rootView: contentView)
+			window.rootViewController = UIHostingController(rootView: contentView)
+			window.windowLevel = .normal
 			
 			// Set the app-wide tint/accent color scheme.
 			//
@@ -58,7 +72,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 			Prefs.shared.themePublisher.sink {[weak self](theme: Theme) in
 				
 				self?.window?.overrideUserInterfaceStyle = theme.toInterfaceStyle()
-				self?.window?.tintColor = UIColor.appAccent // might be customized for color theme
+				self?.window?.tintColor = UIColor.appAccent // appAccent color might be customized for theme
 				
 			}.store(in: &cancellables)
 			
@@ -82,9 +96,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 				AppDelegate.get().externalLightningUrlPublisher.send(url)
 			}
 		}
+		
+		if isAppLaunch {
+			isAppLaunch = false
+			onAppLaunch()
+		}
 	}
 
 	func sceneDidDisconnect(_ scene: UIScene) {
+		log.trace("sceneDidDisconnect()")
+		
 		// Called as the scene is being released by the system.
 		// This occurs shortly after the scene enters the background, or when its session is discarded.
 		// Release any resources associated with this scene that can be re-created the next time the
@@ -94,35 +115,47 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 	}
 
 	func sceneDidBecomeActive(_ scene: UIScene) {
+		log.trace("sceneDidBecomeActive()")
+		
 		// Called when the scene has moved from an inactive state to an active state.
 		// Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
 	}
 
 	func sceneWillResignActive(_ scene: UIScene) {
+		log.trace("sceneWillResignActive()")
+		
 		// Called when the scene will move from an active state to an inactive state.
 		// This may occur due to temporary interruptions (ex. an incoming phone call).
 	}
 
 	func sceneWillEnterForeground(_ scene: UIScene) {
+		log.trace("sceneWillEnterForeground()")
+		
 		// Called as the scene transitions from the background to the foreground.
 		// Use this method to undo the changes made on entering the background.
 	}
 
 	func sceneDidEnterBackground(_ scene: UIScene) {
-        
-		// Shortly after this method returns:
-		//
-		// > UIKit takes a snapshot of your app’s current user interface.
-		// > The system displays the resulting image in the app switcher.
-		// > It also displays the image temporarily when bringing your app
-		// > back to the foreground.
-		//
-		// ContentView.onDidEnterBackground() will request a UI update (via a State change).
-		// But it appears that the update will run AFTER the OS takes the screenshot.
-		// So we have to explicitly notify the system than an update is needed / pending.
-		//
-		if let viewController = window?.rootViewController {
-			viewController.view.setNeedsLayout()
+		log.trace("sceneDidEnterBackground()")
+		
+		let currentSecurity = AppSecurity.shared.enabledSecurity.value
+		if !currentSecurity.isEmpty {
+			
+			lockState.isUnlocked = false
+			showLockWindow()
+			
+			// Shortly after this method returns:
+			//
+			// > UIKit takes a snapshot of your app’s current user interface.
+			// > The system displays the resulting image in the app switcher.
+			// > It also displays the image temporarily when bringing your app
+			// > back to the foreground.
+			//
+			// We've requested a UI update (via @State change to lockState).
+			// But it appears that the update will run AFTER the OS takes the screenshot.
+			// So we have to explicitly notify the system than an update is needed / pending.
+			//
+			lockWindow?.rootViewController?.view.setNeedsLayout()
 		}
 	}
 	
@@ -138,6 +171,103 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 		if let context = URLContexts.first {
 			log.debug("openURL: \(context.url)")
 			AppDelegate.get().externalLightningUrlPublisher.send(context.url)
+		}
+	}
+	
+	// --------------------------------------------------
+	// MARK: App Launch
+	// --------------------------------------------------
+	
+	private func onAppLaunch() -> Void {
+		log.trace("onAppLaunch()")
+		
+		// Note that we cannot use `lockState.objectWillChange.sink`,
+		// because the callback is invoked before the lockState is changed,
+		// and doesn't provide us with the new value.
+		//
+		lockState.$isUnlocked.sink {[weak self] isUnlocked in
+			
+			log.debug("lockState.isUnlocked = \(isUnlocked)")
+			if isUnlocked {
+				self?.hideLockWindow()
+			}
+		}.store(in: &cancellables)
+		
+		AppSecurity.shared.tryUnlockWithKeychain {(mnemonics: [String]?, enabledSecurity: EnabledSecurity) in
+
+			// There are multiple potential configurations:
+			//
+			// - no security       => mnemonics are available, enabledSecurity is empty
+			// - standard security => mnemonics are available, enabledSecurity is non-empty
+			// - advanced security => mnemonics are not available, enabledSecurity is non-empty
+			//
+			// Another way to think about it:
+			// - standard security => touchID only protects the UI, wallet can immediately be loaded
+			// - advanced security => touchID required to unlock both the UI and the seed
+
+			if let mnemonics = mnemonics {
+				// unlock & load wallet
+				AppDelegate.get().loadWallet(mnemonics: mnemonics)
+			}
+			
+			if enabledSecurity.isEmpty {
+				self.lockState.isUnlocked = true
+			} else {
+				self.showLockWindow()
+			}
+		}
+	}
+	
+	// --------------------------------------------------
+	// MARK: App Switcher Privacy
+	// --------------------------------------------------
+	
+	private func showLockWindow() {
+		log.trace("showLockWindow()")
+		
+		guard let windowScene = self.window?.windowScene else {
+			return
+		}
+		
+		if lockWindow == nil {
+			
+			let lockView = LockView(lockState: lockState)
+			
+			let controller = UIHostingController(rootView: lockView)
+			controller.view.backgroundColor = .clear
+			
+			lockWindow = UIWindow(windowScene: windowScene)
+			lockWindow?.rootViewController = controller
+			lockWindow?.tintColor = UIColor.appAccent
+			lockWindow?.overrideUserInterfaceStyle = Prefs.shared.theme.toInterfaceStyle()
+			lockWindow?.windowLevel = .alert + 1
+		}
+		
+		lockWindow?.makeKeyAndVisible()
+	}
+	
+	private func hideLockWindow() {
+		log.trace("hideLockWindow()")
+		
+		// Make window the responder for touch events & other input.
+		window?.makeKey()
+		
+		// The LockView uses an animation to dismiss the lock screen.
+		// So we wait until after the animation has completed,
+		// and then we cleanup our resources (if still needed).
+		//
+		DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {[weak self] in
+			
+			guard let self = self else {
+				return
+			}
+			if self.lockState.isUnlocked {
+				
+				log.debug("Cleaning up lockWindow resources...")
+				
+				self.lockWindow?.isHidden = true
+				self.lockWindow = nil
+			}
 		}
 	}
 }
