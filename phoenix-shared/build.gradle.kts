57791fe4d98d4c6cbcf80fb3268ebac45b7161f2
Dominique <5765435+dpad85@users.noreply.github.com>
2021-05-14 20:07:54 +0200

    Use lightning-kmp v1.0-beta12

--- phoenix-shared/build.gradle.kts
+++ phoenix-shared/build.gradle.kts
@@ -64,7 +64,7 @@ kotlin {
 
     sourceSets {
 
-        val lightningkmpVersion = "snapshot"
+        val lightningkmpVersion = "1.0-beta12"
         val coroutinesVersion = "1.4.2-native-mt"
         val serializationVersion = "1.1.0"
         val secp256k1Version = "0.4.1"
