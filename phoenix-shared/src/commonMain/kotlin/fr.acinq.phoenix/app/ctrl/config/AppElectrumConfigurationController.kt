704ea1f95b1cd16eaa9eba6f0e36c313f8f3e6b6
Robbie Hanson <robbiehanson@deusty.com>
2021-08-04 10:51:43 -0500

    Fixing compiler warning about unused variable

--- phoenix-shared/src/commonMain/kotlin/fr.acinq.phoenix/app/ctrl/config/AppElectrumConfigurationController.kt
+++ phoenix-shared/src/commonMain/kotlin/fr.acinq.phoenix/app/ctrl/config/AppElectrumConfigurationController.kt
@@ -58,20 +58,26 @@ class AppElectrumConfigurationController(
     override fun process(intent: ElectrumConfiguration.Intent) {
         when (intent) {
             is ElectrumConfiguration.Intent.UpdateElectrumServer -> {
-                var error: Error? = null
-                when {
-                    intent.address.isNullOrBlank() -> configurationManager.updateElectrumConfig(null)
-                    !intent.address.matches("""(.*):(\d*)""".toRegex()) -> {
-                        error = InvalidElectrumAddress
+                val error: Error? = when {
+                    intent.address.isNullOrBlank() -> {
+                        configurationManager.updateElectrumConfig(null)
+                        null
                     }
-                    else -> {
+                    intent.address.matches("""(.*):(\d*)""".toRegex()) -> {
                         intent.address.split(":").let {
                             val host = it.first()
                             val port = it.last()
                             val serverAddress = ServerAddress(host, port.toInt(), TcpSocket.TLS.SAFE)
                             configurationManager.updateElectrumConfig(serverAddress)
                         }
+                        null
                     }
+                    else -> {
+                        InvalidElectrumAddress
+                    }
+                }
+                launch {
+                    model { copy(error = error) }
                 }
             }
         }
