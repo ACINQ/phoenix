f035249b33916aaba43b11ba9de944aafe2c9829
Robbie Hanson <robbiehanson@deusty.com>
2021-08-05 15:55:46 -0500

    Adding ShortSheet as a new UI option

--- phoenix-ios/phoenix-ios/ContentView.swift
+++ phoenix-ios/phoenix-ios/ContentView.swift
@@ -30,6 +30,9 @@ struct ContentView: MVIView {
 	@ObservedObject var lockState: LockState
 	@State var unlockedOnce = false
 	
+	@Environment(\.shortSheetState) private var shortSheetState: ShortSheetState
+	@State private var shortSheetItem: ShortSheetItem? = nil
+	
 	@Environment(\.popoverState) private var popoverState: PopoverState
 	@State private var popoverItem: PopoverItem? = nil
 	
@@ -46,11 +49,18 @@ struct ContentView: MVIView {
 						unlockedOnce = true
 					}
 
+				if let shortSheetItem = shortSheetItem {
+					ShortSheetWrapper(dismissable: shortSheetItem.dismissable) {
+						shortSheetItem.view
+					}
+					.zIndex(1) // needed for proper animation
+				}
+				
 				if let popoverItem = popoverItem {
 					PopoverWrapper(dismissable: popoverItem.dismissable) {
 						popoverItem.view
 					}
-					.zIndex(1) // needed for proper animation
+					.zIndex(2) // needed for proper animation
 				}
 			
 			} else { // prior to first unlock
@@ -58,7 +68,7 @@ struct ContentView: MVIView {
 				NavigationView {
 					loadingView()
 				}
-				.zIndex(2) // needed for proper animation
+				.zIndex(3) // needed for proper animation
 				.transition(.asymmetric(
 					insertion : .identity,
 					removal   : .opacity
@@ -67,6 +77,11 @@ struct ContentView: MVIView {
 			
 		} // </ZStack>
 		.modifier(GlobalEnvironment())
+		.onReceive(shortSheetState.publisher) { (item: ShortSheetItem?) in
+			withAnimation {
+				shortSheetItem = item
+			}
+		}
 		.onReceive(popoverState.publisher) { (item: PopoverItem?) in
 			withAnimation {
 				popoverItem = item
---
+++ phoenix-ios/phoenix-ios/views/widgets/ShortSheet.swift
@@ -0,0 +1,135 @@
+import SwiftUI
+import Combine
+
+/// The ShortSheetState is exposed via an Environment variable:
+/// ```
+/// @Environment(\.shortSheetState) var shortSheetState: ShortSheetState
+/// ```
+///
+/// When you want to display a short sheet:
+/// ```
+/// shortSheetState.display(dismissable: false) {
+///    YourCustomView()
+/// }
+/// ```
+///
+/// When you want to dismiss the sheet:
+/// ```
+/// shortSheetState.close()
+/// ```
+///
+public class ShortSheetState: ObservableObject {
+
+	var publisher = PassthroughSubject<ShortSheetItem?, Never>()
+	
+	fileprivate var closePublisher = PassthroughSubject<Void, Never>()
+	
+	func display<Content: View>(dismissable: Bool, @ViewBuilder builder: () -> Content) {
+		publisher.send(ShortSheetItem(
+			dismissable: dismissable,
+			view: builder().anyView
+		))
+
+	}
+	
+	func close() {
+		closePublisher.send()
+	}
+	
+	func close(animationCompletion: @escaping () -> Void) {
+		
+		var cancellables = Set<AnyCancellable>()
+		publisher.sink { (item: ShortSheetItem?) in
+			
+			animationCompletion()
+			cancellables.removeAll()
+			
+		}.store(in: &cancellables)
+		
+		closePublisher.send()
+	}
+}
+
+/// Encompasses the view & options for the popover.
+///
+public struct ShortSheetItem {
+	
+	/// Whether or not the popover is dimissable by clicking outside the popover.
+	let dismissable: Bool
+	
+	/// The view to be displayed in the sheet.
+	/// (Use the View.anyView extension function.)
+	let view: AnyView
+}
+
+struct ShortSheetEnvironmentKey: EnvironmentKey {
+
+	static var defaultValue = ShortSheetState()
+}
+
+public extension EnvironmentValues {
+
+	var shortSheetState: ShortSheetState {
+		get { self[ShortSheetEnvironmentKey.self] }
+		set { self[ShortSheetEnvironmentKey.self] = newValue }
+	}
+}
+
+struct ShortSheetWrapper<Content: View>: View {
+
+	let dismissable: Bool
+	let content: () -> Content
+	
+	@State var animation: CGFloat = 0.0
+	
+	@Environment(\.shortSheetState) private var shortSheetState: ShortSheetState
+	
+	var body: some View {
+		
+		ZStack {
+			if animation == 1 {
+				Color.primary.opacity(0.4)
+					.edgesIgnoringSafeArea(.all)
+					.zIndex(0)
+					.transition(.opacity)
+					.onTapGesture {
+						if dismissable {
+							shortSheetState.close()
+						}
+					}
+				
+				VStack {
+					Spacer()
+					content()
+						.background(
+							Color(UIColor.systemBackground)
+								.cornerRadius(15, corners: [.topLeft, .topRight])
+								.edgesIgnoringSafeArea([.horizontal, .bottom])
+						)
+				}
+				.zIndex(1)
+				.transition(.move(edge: .bottom))
+			}
+		}
+		.transition(.identity)
+		.onAppear {
+			withAnimation {
+				animation = 1
+			}
+		}
+		.onReceive(shortSheetState.closePublisher) { _ in
+			withAnimation {
+				animation = 2
+			}
+		}
+		.onAnimationCompleted(for: animation) {
+			animationCompleted()
+		}
+	}
+	
+	func animationCompleted() {
+		if animation == 2 {
+			shortSheetState.publisher.send(nil)
+		}
+	}
+}
