ca48a877b00bc8269330502494d6fcffed6f127e
Robbie Hanson <robbiehanson@deusty.com>
2021-08-23 10:15:58 -0500

    Adding explanation to "bitcoin unit" selection screen.

--- phoenix-ios/phoenix-ios/views/configuration/general/DisplayConfigurationView.swift
+++ phoenix-ios/phoenix-ios/views/configuration/general/DisplayConfigurationView.swift
@@ -38,15 +38,17 @@ struct DisplayConfigurationView: View {
 					}
 				}
 				
-				Picker(
-					selection: Binding(
-						get: { bitcoinUnit },
-						set: { Prefs.shared.bitcoinUnit = $0 }
-					), label: Text("Bitcoin unit")
+				NavigationLink(
+					destination: BitcoinUnitSelector(selectedBitcoinUnit: bitcoinUnit)
 				) {
-					ForEach(0 ..< BitcoinUnit.default().values.count) {
-						let bitcoinUnit = BitcoinUnit.default().values[$0]
-						bitcoinUnitText(bitcoinUnit).tag(bitcoinUnit)
+					HStack(alignment: VerticalAlignment.firstTextBaseline, spacing: 0) {
+						Text("Bitcoin unit")
+						Spacer()
+						Text(bitcoinUnit.shortName)
+							.foregroundColor(Color.secondary) +
+						Text(verbatim: "  \(bitcoinUnit.explanation)")
+							.font(.footnote)
+							.foregroundColor(Color.secondary)
 					}
 				}
 				
@@ -92,31 +94,12 @@ struct DisplayConfigurationView: View {
 		}
 	}
 	
-	@ViewBuilder
-	func fiatCurrencyText(_ fiatCurrency: FiatCurrency) -> some View {
-		
-		Text(fiatCurrency.shortName) +
-		Text(verbatim: "  \(fiatCurrency.longName)")
-			.font(.footnote)
-			.foregroundColor(Color.secondary)
-	}
-	
-	@ViewBuilder
-	func bitcoinUnitText(_ bitcoinUnit: BitcoinUnit) -> some View {
-		
-		// TODO: Define explanation of what a bitcoin unit is client side
-		Text(bitcoinUnit.shortName) +
-		Text(verbatim: "  \(bitcoinUnit.explanation)")
-			.font(.footnote)
-			.foregroundColor(Color.secondary)
-	}
-	
 	func onAppear() {
 		log.trace("onAppear()")
 		
 		// SwiftUI BUG, and workaround.
 		//
-		// In iOS 14, the Picker remains selected after we return from the subview.
+		// In iOS 14, the row remains selected after we return from the subview.
 		// For example:
 		// - Tap on "Fiat Currency"
 		// - Make a selection or tap "<" to pop back
@@ -161,6 +144,8 @@ struct FiatCurrencySelector: View, ViewName {
 		
 		ZStack {
 			
+			let fiatCurrencies = FiatCurrency.default().values
+			
 			// We want to vertically align the text:
 			//
 			// AUD (flag) Australian Dollar
@@ -177,8 +162,8 @@ struct FiatCurrencySelector: View, ViewName {
 			//
 			ScrollView {
 				VStack {
-					ForEach(0 ..< FiatCurrency.default().values.count) {
-						let fiatCurrency = FiatCurrency.default().values[$0]
+					ForEach(0 ..< fiatCurrencies.count) {
+						let fiatCurrency = fiatCurrencies[$0]
 
 						Text(fiatCurrency.shortName)
 							.foregroundColor(Color.clear)
@@ -196,8 +181,8 @@ struct FiatCurrencySelector: View, ViewName {
 			}
 			
 			List {
-				ForEach(0 ..< FiatCurrency.default().values.count) {
-					let fiatCurrency = FiatCurrency.default().values[$0]
+				ForEach(0 ..< fiatCurrencies.count) {
+					let fiatCurrency = fiatCurrencies[$0]
 					
 					Button {
 						didSelect(fiatCurrency)
@@ -224,7 +209,7 @@ struct FiatCurrencySelector: View, ViewName {
 				
 			Text(fiatCurrency.flag)
 				.frame(width: flagWidth, alignment: .center)
-				.padding(.leading, 2)
+				.padding(.leading, 3)
 			
 			Text(verbatim: "  \(fiatCurrency.longName)")
 				.font(.footnote)
@@ -244,6 +229,101 @@ struct FiatCurrencySelector: View, ViewName {
 		
 		selectedFiatCurrency = fiatCurrency
 		Prefs.shared.fiatCurrency = fiatCurrency
+		
+		DispatchQueue.main.asyncAfter(deadline: .now() + 0.10) {
+			presentationMode.wrappedValue.dismiss()
+		}
+	}
+}
+
+struct BitcoinUnitSelector: View, ViewName {
+	
+	@State var selectedBitcoinUnit: BitcoinUnit
+	
+	@Environment(\.colorScheme) var colorScheme
+	@Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+	
+	@ViewBuilder
+	var body: some View {
+		
+		VStack(alignment: HorizontalAlignment.center, spacing: 0) {
+			
+			List {
+				let bitcoinUnits = BitcoinUnit.default().values
+				ForEach(0 ..< bitcoinUnits.count) {
+					let bitcoinUnit = bitcoinUnits[$0]
+					Button {
+						didSelect(bitcoinUnit)
+					} label: {
+						row(bitcoinUnit)
+					}
+				}
+			}
+			.listStyle(PlainListStyle())
+			
+			footer()
+				.padding(.horizontal, 10)
+				.padding(.vertical, 20)
+				.frame(maxWidth: .infinity)
+				.background(
+					Color(
+						colorScheme == ColorScheme.light
+						? UIColor.systemGroupedBackground
+						: UIColor.secondarySystemGroupedBackground
+					)
+					.edgesIgnoringSafeArea(.bottom) // background color should extend to bottom of screen
+				)
+		}
+		.navigationBarTitle(
+			NSLocalizedString("Bitcoin unit", comment: "Navigation bar title"),
+			displayMode: .inline
+		)
+	}
+	
+	@ViewBuilder
+	func row(_ bitcoinUnit: BitcoinUnit) -> some View {
+		
+		HStack(alignment: VerticalAlignment.firstTextBaseline, spacing: 0) {
+			
+			Text(bitcoinUnit.shortName)
+			
+			Spacer()
+			
+			Text(verbatim: "  \(bitcoinUnit.explanation)")
+				.font(.footnote)
+				.foregroundColor(Color.secondary)
+				.padding(.trailing, 4)
+			
+			let isSelected = bitcoinUnit == selectedBitcoinUnit
+			Image(systemName: "checkmark")
+				.foregroundColor(isSelected ? .appAccent : .clear)
+		}
+	}
+	
+	@ViewBuilder
+	func footer() -> some View {
+		
+		VStack(alignment: HorizontalAlignment.leading, spacing: 10) {
+			Text(
+				"""
+				A bitcoin can be divided into smaller units. The smallest unit is called a \
+				Satoshi, and there are 100 million satoshis in a single bitcoin.
+				"""
+			)
+			Text(
+				"""
+				Satoshis are often preferred because it's easier to work with whole numbers rather than fractions.
+				"""
+			)
+		}
+		.font(.callout)
+	}
+	
+	func didSelect(_ bitcoinUnit: BitcoinUnit) {
+		log.trace("didSelect(bitcoinUnit = \(bitcoinUnit.shortName)")
+		
+		selectedBitcoinUnit = bitcoinUnit
+		Prefs.shared.bitcoinUnit = bitcoinUnit
 		
 		DispatchQueue.main.asyncAfter(deadline: .now() + 0.10) {
 			presentationMode.wrappedValue.dismiss()
