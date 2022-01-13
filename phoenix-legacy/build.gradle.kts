691ab82e605059a45cf2bae30048366e81760621
dpad85 <5765435+dpad85@users.noreply.github.com>
2022-01-13 18:18:27 +0100

    (legacy) Increment build code (35)

--- phoenix-legacy/build.gradle.kts
+++ phoenix-legacy/build.gradle.kts
@@ -29,7 +29,7 @@ android {
     applicationId = "fr.acinq.phoenix.testnet"
     minSdk = 24
     targetSdk = 30
-    versionCode = 34
+    versionCode = 35
     versionName = gitCommitHash()
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
