27fe09e948a53f87e74a8706fb2419598088b8f9
Robbie Hanson <robbiehanson@deusty.com>
2022-02-10 11:18:53 -0300

    (ios) Adding ability to delete failed payments (#246)

--- phoenix-shared/src/androidMain/kotlin/fr/acinq/phoenix/db/SqlPaymentHooks.kt
+++ phoenix-shared/src/androidMain/kotlin/fr/acinq/phoenix/db/SqlPaymentHooks.kt
@@ -4,7 +4,7 @@ import fr.acinq.phoenix.data.WalletPaymentId
 import fr.acinq.phoenix.db.payments.CloudKitInterface
 
 actual fun didCompleteWalletPayment(id: WalletPaymentId, database: PaymentsDatabase) {}
-
+actual fun didDeleteWalletPayment(id: WalletPaymentId, database: PaymentsDatabase) {}
 actual fun didUpdateWalletPaymentMetadata(id: WalletPaymentId, database: PaymentsDatabase) {}
 
 actual fun makeCloudKitDb(database: PaymentsDatabase): CloudKitInterface? {
