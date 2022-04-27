cda83f383f3a9a29e5af448bb816b0ba3cbec0d0
dpad85 <5765435+dpad85@users.noreply.github.com>
2022-04-27 12:00:13 +0200

    Update with changes to TcpSocket after certificate pinning

--- phoenix-shared/src/commonMain/kotlin/fr.acinq.phoenix/utils/Connection.kt
+++ phoenix-shared/src/commonMain/kotlin/fr.acinq.phoenix/utils/Connection.kt
@@ -4,6 +4,7 @@ operator fun Connection.plus(other: Connection) : Connection =
     when {
         this == other -> this
         this == Connection.ESTABLISHING || other == Connection.ESTABLISHING -> Connection.ESTABLISHING
-        this == Connection.CLOSED || other == Connection.CLOSED -> Connection.CLOSED
+        this is Connection.CLOSED -> this
+        other is Connection.CLOSED -> other
         else -> error("Cannot add [$this + $other]")
     }
