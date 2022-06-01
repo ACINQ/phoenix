717d86e2dd50f8ce337b8a7b1b8337bb2bae7afc
Nicolas Fillion <nfillion21@gmail.com>
2022-06-01 15:56:10 +0200

    Clean imports

--- phoenix-android/src/main/kotlin/fr/acinq/phoenix/android/init/RestoreDisclaimerView.kt
+++ phoenix-android/src/main/kotlin/fr/acinq/phoenix/android/init/RestoreDisclaimerView.kt
@@ -16,36 +16,26 @@
 
 package fr.acinq.phoenix.android.init
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Checkbox
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import fr.acinq.phoenix.android.CF
 import fr.acinq.phoenix.android.R
-import fr.acinq.phoenix.android.components.*
-import fr.acinq.phoenix.android.components.mvi.MVIView
+import fr.acinq.phoenix.android.components.BorderButton
+import fr.acinq.phoenix.android.components.InitHeader
+import fr.acinq.phoenix.android.components.InitScreen
 import fr.acinq.phoenix.android.controllerFactory
 import fr.acinq.phoenix.android.navController
-import fr.acinq.phoenix.android.security.KeyState
-import fr.acinq.phoenix.android.security.SeedManager
-import fr.acinq.phoenix.android.utils.logger
-import fr.acinq.phoenix.controllers.init.RestoreWallet
 
 @Composable
 fun RestoreDisclaimerView(
