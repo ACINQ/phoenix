f2e797070c89746c863d96c045d5f4e95eb9dfb3
Dominique <5765435+dpad85@users.noreply.github.com>
2022-10-07 14:33:26 +0200

    Support for new swap-in events
    The swap-in address now belongs to the wallet. It is generated
    by lightning-kmp using an address on account 1.
    
    The swap-in flow has also changed. Swap-ins can be accepted or
    rejected by Phoenix or the peer (for example if the fee is too
    low or the swapped amount below a given threshold). There are
    new events for channel creation (that are triggered by swap-ins
    but also by regular pay-to-opens).

--- phoenix-shared/src/commonMain/kotlin/fr.acinq.phoenix/managers/NodeParamsManager.kt
+++ phoenix-shared/src/commonMain/kotlin/fr.acinq.phoenix/managers/NodeParamsManager.kt
@@ -29,7 +29,6 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import org.kodein.log.LoggerFactory
 import org.kodein.log.newLogger
--- phoenix-shared/src/commonMain/kotlin/fr.acinq.phoenix/managers/PaymentsManager.kt
+++ phoenix-shared/src/commonMain/kotlin/fr.acinq.phoenix/managers/PaymentsManager.kt
@@ -1,19 +1,29 @@
 package fr.acinq.phoenix.managers
 
+import fr.acinq.lightning.ChannelEvents
 import fr.acinq.lightning.MilliSatoshi
+import fr.acinq.lightning.SwapInEvents
 import fr.acinq.lightning.db.WalletPayment
-import fr.acinq.lightning.io.*
+import fr.acinq.lightning.io.PaymentNotSent
+import fr.acinq.lightning.io.PaymentProgress
+import fr.acinq.lightning.io.PaymentSent
 import fr.acinq.lightning.utils.UUID
 import fr.acinq.lightning.utils.getValue
 import fr.acinq.lightning.utils.setValue
 import fr.acinq.lightning.utils.toMilliSatoshi
 import fr.acinq.phoenix.PhoenixBusiness
-import fr.acinq.phoenix.data.*
+import fr.acinq.phoenix.data.WalletPaymentFetchOptions
+import fr.acinq.phoenix.data.WalletPaymentId
+import fr.acinq.phoenix.data.WalletPaymentInfo
+import fr.acinq.phoenix.data.WalletPaymentMetadata
 import fr.acinq.phoenix.db.SqlitePaymentsDb
-import fr.acinq.phoenix.db.WalletPaymentOrderRow
-import kotlinx.coroutines.*
-import kotlinx.coroutines.channels.consumeEach
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 import org.kodein.log.LoggerFactory
 import org.kodein.log.newLogger
 
@@ -21,13 +31,15 @@ import org.kodein.log.newLogger
 class PaymentsManager(
     private val loggerFactory: LoggerFactory,
     private val peerManager: PeerManager,
-    private val databaseManager: DatabaseManager
+    private val databaseManager: DatabaseManager,
+    private val nodeParamsManager: NodeParamsManager,
 ) : CoroutineScope by MainScope() {
 
     constructor(business: PhoenixBusiness): this(
         loggerFactory = business.loggerFactory,
         peerManager = business.peerManager,
-        databaseManager = business.databaseManager
+        databaseManager = business.databaseManager,
+        nodeParamsManager = business.nodeParamsManager,
     )
 
     private val log = newLogger(loggerFactory)
@@ -120,26 +132,44 @@ class PaymentsManager(
                     is PaymentNotSent -> {
                         removeFromInFlightOutgoingPayments(event.request.paymentId)
                     }
-                    is SwapInPendingEvent -> {
-                        _incomingSwapsMap += (event.swapInPending.bitcoinAddress to event.swapInPending.amount.toMilliSatoshi())
-                    }
-                    is SwapInConfirmedEvent -> {
-                        _incomingSwapsMap -= event.swapInConfirmed.bitcoinAddress
-                    }
                     else -> Unit
+                }
+            }
+        }
+        launch {
+            nodeParamsManager.nodeParams.filterNotNull().first().nodeEvents.collect {
+                when (it) {
+                    is SwapInEvents.Requested -> {
+                        // for now, we use a placeholder address because it's not yet exposed by the lightning-kmp API
+                        _incomingSwapsMap = _incomingSwapsMap + ("foobar" to it.req.localFundingAmount.toMilliSatoshi())
+                    }
+                    is SwapInEvents.Accepted -> {
+                        log.info { "swap-in request=${it.requestId} has been accepted for funding_fee=${it.fundingFee} service_fee=${it.serviceFee}" }
+                    }
+                    is SwapInEvents.Rejected -> {
+                        log.error { "rejected swap-in for required_fee=${it.requiredFees} with error=${it.failure}" }
+                        _incomingSwapsMap = _incomingSwapsMap - "foobar"
+                    }
+                    is ChannelEvents.Creating -> {
+                        log.info { "channel=${it.state.channelId} is being created" }
+                    }
+                    is ChannelEvents.Created -> {
+                        log.info { "channel=${it.state.channelId} has been successfully created!" }
+                        _incomingSwapsMap = _incomingSwapsMap - "foobar"
+                    }
                 }
             }
         }
     }
 
-    /// Adds to StateFlow<Set<UUID>>
+    /** Adds to StateFlow<Set<UUID>> */
     private fun addToInFlightOutgoingPayments(id: UUID) {
         val oldSet = _inFlightOutgoingPayments.value
         val newSet = oldSet.plus(id)
         _inFlightOutgoingPayments.value = newSet
     }
 
-    /// Removes from StateFlow<Set<UUID>>
+    /** Removes from StateFlow<Set<UUID>> */
     private fun removeFromInFlightOutgoingPayments(id: UUID) {
         val oldSet = _inFlightOutgoingPayments.value
         val newSet = oldSet.minus(id)
