3576d043b4996fec108eec70d1fec824663e8f27
Robbie Hanson <robbiehanson@deusty.com>
2022-10-19 15:19:59 +0200

    Removing dead code

--- phoenix-shared/src/commonMain/kotlin/fr.acinq.phoenix/data/Wallet.kt
+++ phoenix-shared/src/commonMain/kotlin/fr.acinq.phoenix/data/Wallet.kt
@@ -33,13 +33,6 @@ data class Wallet(val seed: ByteVector64, val chain: Chain) {
         return masterPublicKey(masterPubkeyPath) to masterPubkeyPath
     }
 
-    fun onchainAddress(path: String): String {
-        val isMainnet = chain.isMainnet()
-        val chainHash = if (isMainnet) Block.LivenetGenesisBlock.hash else Block.TestnetGenesisBlock.hash
-        val publicKey = DeterministicWallet.derivePrivateKey(master, path).publicKey
-        return Bitcoin.computeBIP84Address(publicKey, chainHash)
-    }
-
     /**
      * We use a separate key for cloud storage.
      * That is, the key we use to encyrpt/decrypt the blobs we store in the cloud.
