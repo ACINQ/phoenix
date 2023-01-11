cc9f7b72ca84515cb49dfb83bb9e2ffb2c43f859
Dominique <5765435+dpad85@users.noreply.github.com>
2023-01-11 18:24:47 +0100

    (android) Add version in about

--- phoenix-android/src/main/kotlin/fr/acinq/phoenix/android/settings/AboutView.kt
+++ phoenix-android/src/main/kotlin/fr/acinq/phoenix/android/settings/AboutView.kt
@@ -20,13 +20,14 @@ package fr.acinq.phoenix.android.settings
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import fr.acinq.phoenix.android.BuildConfig
 import fr.acinq.phoenix.android.R
 import fr.acinq.phoenix.android.components.*
 import fr.acinq.phoenix.android.navController
@@ -54,6 +55,12 @@ fun AboutView() {
             Text(text = annotatedStringResource(id = R.string.about_rates_title), style = MaterialTheme.typography.h5)
             Spacer(modifier = Modifier.height(10.dp))
             Text(text = annotatedStringResource(id = R.string.about_rates_content))
+        }
+        Card {
+            Text(
+                text = annotatedStringResource(id = R.string.about_version, BuildConfig.VERSION_NAME),
+                modifier = Modifier.fillMaxWidth().padding(16.dp)
+            )
         }
         Card {
             SettingButton(
