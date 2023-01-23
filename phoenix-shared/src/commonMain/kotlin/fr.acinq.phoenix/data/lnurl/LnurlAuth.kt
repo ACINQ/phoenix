473685717fc967936987a7daefeff50ef04fca13
Dominique <5765435+dpad85@users.noreply.github.com>
2023-01-23 17:28:11 +0100

    Add bolt.fun as a legacy lnurl-auth domain
    The changes on lnurl-auth to use full domain names broke
    accounts on bolt.fun. The legacy scheme should be used.
    
    Fixes #328

--- phoenix-shared/src/commonMain/kotlin/fr.acinq.phoenix/data/lnurl/LnurlAuth.kt
+++ phoenix-shared/src/commonMain/kotlin/fr.acinq.phoenix/data/lnurl/LnurlAuth.kt
@@ -151,6 +151,7 @@ data class LnurlAuth(
         // COINOS("", ""),
         LNSHORT("lnshort.it", "lnshort.it"),
         STACKERNEWS("stacker.news", "stacker.news"),
+        BOLTFUN("auth.bolt.fun", "bolt.fun")
         ;
 
         companion object {
