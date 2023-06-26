522ee03b876430abdc4a11aea6a3b0c58aaabf67
Robbie Hanson <304604+robbiehanson@users.noreply.github.com>
2023-06-26 12:30:46 -0500

    (ios) Adding preimage information to payment details screen (along with invoice description & raw invoice)

--- phoenix-ios/phoenix-ios/views/inspect/DetailsView.swift
+++ phoenix-ios/phoenix-ios/views/inspect/DetailsView.swift
@@ -123,8 +123,11 @@ fileprivate struct DetailsInfoGrid: InfoGridView {
 			header(NSLocalizedString("Payment Request", comment: "Title in DetailsView_IncomingPayment"))
 		
 			paymentRequest_invoiceCreated(paymentRequest)
+			paymentRequest_invoiceDescription(paymentRequest)
 			paymentRequest_amountRequested(paymentRequest)
 			paymentRequest_paymentHash(paymentRequest)
+			paymentRequest_preimage()
+			paymentRequest_invoice(paymentRequest)
 		}
 		
 		if let received = incomingPayment.received {
@@ -162,8 +165,11 @@ fileprivate struct DetailsInfoGrid: InfoGridView {
 			header(NSLocalizedString("Payment Request", comment: "Title in DetailsView_IncomingPayment"))
 		
 			paymentRequest_invoiceCreated(paymentRequest)
+			paymentRequest_invoiceDescription(paymentRequest)
 			paymentRequest_amountRequested(paymentRequest)
 			paymentRequest_paymentHash(paymentRequest)
+			paymentRequest_preimage()
+			paymentRequest_invoice(paymentRequest)
 		
 		} else if let swapOut = outgoingPayment.details.asSwapOut() {
 			
@@ -383,6 +389,29 @@ fileprivate struct DetailsInfoGrid: InfoGridView {
 	}
 	
 	@ViewBuilder
+	func paymentRequest_invoiceDescription(_ paymentRequest: Lightning_kmpPaymentRequest) -> some View {
+		let identifier: String = #function
+		
+		InfoGridRowWrapper(
+			identifier: identifier,
+			hSpacing: horizontalSpacingBetweenColumns,
+			keyColumnWidth: keyColumnWidth(identifier: identifier)
+		) {
+			
+			keyColumn(NSLocalizedString("invoice description", comment: "Label in DetailsView_IncomingPayment"))
+			
+		} valueColumn: {
+			
+			let description = (paymentRequest.description_ ?? "").trimmingCharacters(in: .whitespacesAndNewlines)
+			if description.isEmpty {
+				Text("empty").foregroundColor(.secondary)
+			} else {
+				Text(description)
+			}
+		}
+	}
+	
+	@ViewBuilder
 	func paymentRequest_amountRequested(_ paymentRequest: Lightning_kmpPaymentRequest) -> some View {
 		let identifier: String = #function
 		
@@ -431,6 +460,60 @@ fileprivate struct DetailsInfoGrid: InfoGridView {
 					}
 				}
 		}
+	}
+	
+	@ViewBuilder
+	func paymentRequest_preimage() -> some View {
+		let identifier: String = #function
+		
+		if let preimage = paymentPreimage() {
+			
+			InfoGridRowWrapper(
+				identifier: identifier,
+				hSpacing: horizontalSpacingBetweenColumns,
+				keyColumnWidth: keyColumnWidth(identifier: identifier)
+			) {
+				keyColumn(NSLocalizedString("payment preimage", comment: "Label in DetailsView_IncomingPayment"))
+				
+			} valueColumn: {
+				
+				Text(preimage)
+					.contextMenu {
+						Button(action: {
+							UIPasteboard.general.string = preimage
+						}) {
+							Text("Copy")
+						}
+					}
+			} // </InfoGridRowWrapper>
+		}
+	}
+
+	@ViewBuilder
+	func paymentRequest_invoice(_ paymentRequest: Lightning_kmpPaymentRequest) -> some View {
+		let identifier: String = #function
+		
+		InfoGridRowWrapper(
+			identifier: identifier,
+			hSpacing: horizontalSpacingBetweenColumns,
+			keyColumnWidth: keyColumnWidth(identifier: identifier)
+		) {
+			keyColumn(NSLocalizedString("invoice", comment: "Label in DetailsView_IncomingPayment"))
+			
+		} valueColumn: {
+			
+			let invoice = paymentRequest.write()
+			Text(invoice)
+				.lineLimit(5)
+				.truncationMode(.tail)
+				.contextMenu {
+					Button(action: {
+						UIPasteboard.general.string = invoice
+					}) {
+						Text("Copy")
+					}
+				}
+		} // </InfoGridRowWrapper>
 	}
 	
 	@ViewBuilder
@@ -1174,6 +1257,22 @@ fileprivate struct DetailsInfoGrid: InfoGridView {
 		
 		return formatter.string(from: NSNumber(value: percent)) ?? "?%"
 	}
+	
+	func paymentPreimage() -> String? {
+		
+		if let incomingPayment = paymentInfo.payment as? Lightning_kmpIncomingPayment {
+			return incomingPayment.preimage.toHex()
+		}
+		
+		if let outgoingPayment = paymentInfo.payment as? Lightning_kmpOutgoingPayment,
+			let offChain = outgoingPayment.status.asOffChain()
+		{
+			return offChain.preimage.toHex()
+		}
+		
+		return nil
+	}
+
 	
 	struct DisplayAmounts {
 		let bitcoin: FormattedAmount
