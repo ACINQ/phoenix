7502ea99611381ff9549270be5afa4ad47f3f2ee
Dominique Padiou <5765435+dpad85@users.noreply.github.com>
2023-06-27 14:55:28 +0200

    (android) Fix text resource

--- phoenix-android/src/main/kotlin/fr/acinq/phoenix/android/settings/walletinfo/WalletInfoView.kt
+++ phoenix-android/src/main/kotlin/fr/acinq/phoenix/android/settings/walletinfo/WalletInfoView.kt
@@ -182,7 +182,7 @@ private fun OnchainBalanceView(
                 unconfirmed.takeUnless { it == 0.sat }?.let {
                     Spacer(modifier = Modifier.width(8.dp))
                     Text(
-                        text = stringResource(id = R.string.walletinfo_incoming_balance, it.toPrettyString(btcUnit, withUnit = true)",
+                        text = stringResource(id = R.string.walletinfo_incoming_balance, it.toPrettyString(btcUnit, withUnit = true)),
                         style = MaterialTheme.typography.caption.copy(fontSize = 14.sp),
                         modifier = Modifier.alignByBaseline(),
                     )
