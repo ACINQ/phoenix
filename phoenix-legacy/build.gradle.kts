3ae813ab946bc729a2dc3388eaf87c7c9a39ff85
Dominique Padiou <5765435+dpad85@users.noreply.github.com>
2023-06-29 18:33:26 +0200

    (android-legacy) Release v1.4.27 (code 43)

--- phoenix-legacy/build.gradle.kts
+++ phoenix-legacy/build.gradle.kts
@@ -30,7 +30,7 @@ android {
     minSdk = 24
     targetSdk = 33
     versionCode = 43
-    versionName = gitCommitHash()
+    versionName = "1.4.27"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
   buildTypes {
