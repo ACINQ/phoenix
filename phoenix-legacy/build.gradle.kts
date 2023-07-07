1d018a72a8e6f3892d0692aa92e133921926dde9
Dominique Padiou <5765435+dpad85@users.noreply.github.com>
2023-07-07 16:11:18 +0200

    (android-legacy) Fix library code increment (52)

--- phoenix-legacy/build.gradle.kts
+++ phoenix-legacy/build.gradle.kts
@@ -31,7 +31,7 @@ android {
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
   buildTypes {
-    val libCode = 50
+    val libCode = 52
     getByName("debug") {
       resValue("string", "CHAIN", chain)
       buildConfigField("String", "CHAIN", chain)
