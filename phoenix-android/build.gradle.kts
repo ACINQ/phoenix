6863b41f7185a2c7475466d9d5aa6cc84feaf42a
Dominique Padiou <5765435+dpad85@users.noreply.github.com>
2023-08-28 14:44:07 +0200

    (android) Increment build code (57)

--- phoenix-android/build.gradle.kts
+++ phoenix-android/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "fr.acinq.phoenix.testnet"
         minSdk = 26
         targetSdk = 33
-        versionCode = 56
+        versionCode = 57
         versionName = gitCommitHash()
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
