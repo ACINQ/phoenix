63025fd23d80c9db2ae3cf9d044bc91079e7e3ce
Robbie Hanson <304604+robbiehanson@users.noreply.github.com>
2023-09-01 11:52:46 -0500

    (ios) Fixing VerticalAlignment issues for Toggle's in PaymentsBackupView

--- phoenix-ios/phoenix-ios/utils/CenterTopLineAlignment.swift
+++ phoenix-ios/phoenix-ios/utils/CenterTopLineAlignment.swift
@@ -20,31 +20,6 @@ import SwiftUI
 // |line2|
 // |line3|
 //
-// Here's how it works:
-// - The toggle queries its body for the VerticalAlignment.center value
-// - Our body is our Label
-// - So in `Step A` below, we override the Label's VerticalAlignment.center value to
-//   return instead the VerticalAlignment.centerTopLine value
-// - And in `Step B` below, we provide the value for VerticalAlignment.centerTopLine.
-//
-// Toggle(isOn: $state) {
-//   Label {
-//     VStack(alignment: HorizontalAlignment.leading, spacing: 8) {
-//       Text("line 1")
-//         .alignmentGuide(VerticalAlignment.centerTopLine) { (d: ViewDimensions) in
-//           d[VerticalAlignment.center] // Step B
-//         }
-//       Text("line 2")
-//       Text("line 3")
-//     }
-//   } icon: {
-//     Image(systemName: "network")
-//   } // </Label>
-//   .alignmentGuide(VerticalAlignment.center) { d in
-//     d[VerticalAlignment.centerTopLine] // Step A
-//   }
-// } // </Toggle>
-//
 // A good resource on alignment guides can be found here:
 // https://swiftui-lab.com/alignment-guides/
 
