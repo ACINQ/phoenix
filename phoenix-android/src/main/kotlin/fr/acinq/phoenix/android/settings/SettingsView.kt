bbbc7cbaf99845c4173b2899b1633e2c61290897
Dominique Padiou <5765435+dpad85@users.noreply.github.com>
2023-09-14 15:53:16 +0200

    (android) Merge current & archive files when exporting logs
    On android we use SLF4j to manage logs, with rolling. When exporting
    the logs, we should also export the archive, not only the current
    log file. These two files are now merged in a temp log file when
    exporting logs.
    
    We also stop storing a 2nd archive file, as it is not used.

--- phoenix-android/src/main/kotlin/fr/acinq/phoenix/android/settings/SettingsView.kt
+++ phoenix-android/src/main/kotlin/fr/acinq/phoenix/android/settings/SettingsView.kt
@@ -73,37 +73,37 @@ fun SettingsView() {
         // -- general
         CardHeader(text = stringResource(id = R.string.settings_general_title))
         Card {
-            SettingButton(text = R.string.settings_about, icon = R.drawable.ic_help_circle, onClick = { nc.navigate(Screen.About) })
-            SettingButton(text = R.string.settings_display_prefs, icon = R.drawable.ic_brush, onClick = { nc.navigate(Screen.Preferences) })
-            SettingButton(text = R.string.settings_payment_settings, icon = R.drawable.ic_tool, onClick = { nc.navigate(Screen.PaymentSettings) })
-            SettingButton(text = R.string.settings_liquidity_policy, icon = R.drawable.ic_settings, onClick = { nc.navigate(Screen.LiquidityPolicy) })
-            SettingButton(text = R.string.settings_payment_history, icon = R.drawable.ic_list, onClick = { nc.navigate(Screen.PaymentsHistory) })
-            SettingButton(text = R.string.settings_notifications, icon = R.drawable.ic_notification, onClick = { nc.navigate(Screen.Notifications) })
+            SettingButton(text = stringResource(R.string.settings_about), icon = R.drawable.ic_help_circle, onClick = { nc.navigate(Screen.About) })
+            SettingButton(text = stringResource(R.string.settings_display_prefs), icon = R.drawable.ic_brush, onClick = { nc.navigate(Screen.Preferences) })
+            SettingButton(text = stringResource(R.string.settings_payment_settings), icon = R.drawable.ic_tool, onClick = { nc.navigate(Screen.PaymentSettings) })
+            SettingButton(text = stringResource(R.string.settings_liquidity_policy), icon = R.drawable.ic_settings, onClick = { nc.navigate(Screen.LiquidityPolicy) })
+            SettingButton(text = stringResource(R.string.settings_payment_history), icon = R.drawable.ic_list, onClick = { nc.navigate(Screen.PaymentsHistory) })
+            SettingButton(text = stringResource(R.string.settings_notifications), icon = R.drawable.ic_notification, onClick = { nc.navigate(Screen.Notifications) })
         }
 
         // -- privacy & security
         CardHeader(text = stringResource(id = R.string.settings_security_title))
         Card {
-            SettingButton(text = R.string.settings_access_control, icon = R.drawable.ic_unlock, onClick = { nc.navigate(Screen.AppLock) })
-            SettingButton(text = R.string.settings_display_seed, icon = R.drawable.ic_key, onClick = { nc.navigate(Screen.DisplaySeed) })
-            SettingButton(text = R.string.settings_electrum, icon = R.drawable.ic_chain, onClick = { nc.navigate(Screen.ElectrumServer) })
-            SettingButton(text = R.string.settings_tor, icon = R.drawable.ic_tor_shield, onClick = { nc.navigate(Screen.TorConfig) })
+            SettingButton(text = stringResource(R.string.settings_access_control), icon = R.drawable.ic_unlock, onClick = { nc.navigate(Screen.AppLock) })
+            SettingButton(text = stringResource(R.string.settings_display_seed), icon = R.drawable.ic_key, onClick = { nc.navigate(Screen.DisplaySeed) })
+            SettingButton(text = stringResource(R.string.settings_electrum), icon = R.drawable.ic_chain, onClick = { nc.navigate(Screen.ElectrumServer) })
+            SettingButton(text = stringResource(R.string.settings_tor), icon = R.drawable.ic_tor_shield, onClick = { nc.navigate(Screen.TorConfig) })
         }
 
         // -- advanced
         CardHeader(text = stringResource(id = R.string.settings_advanced_title))
         Card {
-            SettingButton(text = R.string.settings_wallet_info, icon = R.drawable.ic_box, onClick = { nc.navigate(Screen.WalletInfo) })
-            SettingButton(text = R.string.settings_list_channels, icon = R.drawable.ic_zap, onClick = { nc.navigate(Screen.Channels) })
-            SettingButton(text = R.string.settings_logs, icon = R.drawable.ic_text, onClick = { nc.navigate(Screen.Logs) })
+            SettingButton(text = stringResource(R.string.settings_wallet_info), icon = R.drawable.ic_box, onClick = { nc.navigate(Screen.WalletInfo) })
+            SettingButton(text = stringResource(R.string.settings_list_channels), icon = R.drawable.ic_zap, onClick = { nc.navigate(Screen.Channels) })
+            SettingButton(text = stringResource(R.string.settings_logs), icon = R.drawable.ic_text, onClick = { nc.navigate(Screen.Logs) })
         }
         // -- advanced
         CardHeader(text = stringResource(id = R.string.settings_danger_title))
         Card {
 //            SettingButton(text = R.string.settings_reset_wallet, icon = R.drawable.ic_trash, onClick = { nc.navigate(Screen.ResetWallet) })
-            SettingButton(text = R.string.settings_mutual_close, icon = R.drawable.ic_cross_circle, onClick = { nc.navigate(Screen.MutualClose) })
+            SettingButton(text = stringResource(R.string.settings_mutual_close), icon = R.drawable.ic_cross_circle, onClick = { nc.navigate(Screen.MutualClose) })
             SettingButton(
-                text = R.string.settings_force_close,
+                text = stringResource(R.string.settings_force_close),
                 textStyle = MaterialTheme.typography.button.copy(color = negativeColor),
                 icon = R.drawable.ic_alert_triangle,
                 iconTint = negativeColor,
