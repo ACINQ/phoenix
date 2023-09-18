1e357454536cca7906ed884241b39d257b9735fe
Dominique Padiou <5765435+dpad85@users.noreply.github.com>
2023-09-18 15:34:49 +0200

    (android) Release v2.0.7 (code 61)

--- phoenix-android/build.gradle.kts
+++ phoenix-android/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         minSdk = 26
         targetSdk = 33
         versionCode = 61
-        versionName = gitCommitHash()
+        versionName = "2.0.7"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         resourceConfigurations.addAll(listOf("en", "fr", "de", "es", "cs", "pt-rBR"))
     }
