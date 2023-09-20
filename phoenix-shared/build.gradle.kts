d583737ca741da1b21761d66b0f093ec6c0e774f
Robbie Hanson <304604+robbiehanson@users.noreply.github.com>
2023-09-20 10:51:35 -0500

    That specific fix works if you build within Xcode (because Xcode specifically exports that environment variable), but doesn't work if you build on the command line. Or if you try to run unit tests. So I recommend we drop the check, and just say that Xcode 15 is now required to build for iOS.

--- phoenix-shared/build.gradle.kts
+++ phoenix-shared/build.gradle.kts
@@ -37,9 +37,7 @@ kotlin {
                     kotlinOptions.freeCompilerArgs += listOf("-linker-options", "-application_extension")
                     // workaround for xcode 15 and kotlin < 1.9.10: 
                     // https://youtrack.jetbrains.com/issue/KT-60230/Native-unknown-options-iossimulatorversionmin-sdkversion-with-Xcode-15-beta-3
-                    if (System.getenv("XCODE_VERSION_MAJOR") == "1500") {
-                        linkerOpts += "-ld64"
-                    }
+                    linkerOpts += "-ld64"
                 }
             }
         }
