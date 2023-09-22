15e7a6d3760888a0a3e6d821a26b586b4b1a783f
Dominique Padiou <5765435+dpad85@users.noreply.github.com>
2023-09-22 15:10:48 +0200

    Use secp-256k1-kmp 0.10.1

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
     const val lightningKmp = "1.5.6"
-    const val secp256k1 = "0.7.0"
+    const val secp256k1 = "0.10.1"
     const val torMobile = "0.2.0"
 
     const val kotlin = "1.8.21"
