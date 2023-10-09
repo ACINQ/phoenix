838724a20becf085a34452cb2f9e8f544248531d
Dominique Padiou <5765435+dpad85@users.noreply.github.com>
2023-10-09 19:00:21 +0200

    (android) Release 2.0.10 (code 65)

--- phoenix-android/build.gradle.kts
+++ phoenix-android/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         minSdk = 26
         targetSdk = 33
         versionCode = 65
-        versionName = gitCommitHash()
+        versionName = "2.0.10"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         resourceConfigurations.addAll(listOf("en", "fr", "de", "es", "cs", "pt-rBR"))
     }
