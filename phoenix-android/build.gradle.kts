b662f03563f23f5b99bc8c618dbbc5061da4aae8
Dominique Padiou <5765435+dpad85@users.noreply.github.com>
2023-12-12 15:21:41 +0100

    (android) Release 2.0.15 (code 70)

--- phoenix-android/build.gradle.kts
+++ phoenix-android/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         minSdk = 26
         targetSdk = 33
         versionCode = 70
-        versionName = gitCommitHash()
+        versionName = "2.0.15"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         resourceConfigurations.addAll(listOf("en", "fr", "de", "es", "b+es+419", "cs", "pt-rBR"))
     }
