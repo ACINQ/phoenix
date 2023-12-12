ACINQ/phoenix

commit f0ae90444a23cc17d6d7407dfe43c0c8d20e62fc

(android) Use the complete deeplink data when starting scanview
Uri parameters are pruned from the `data` navigation arg. That
is an issue for bip-21 uris with a LN parameter.

Fixes #466

phoenix-android/src/main/kotlin/fr/acinq/phoenix/android/AppView.kt
@@ -17,6 +17,7 @@
 package fr.acinq.phoenix.android
 
 import android.Manifest
+import android.content.Intent
 import android.net.Uri
 import android.os.Build
 import android.text.format.DateUtils
@@ -237,10 +238,10 @@ fun AppView(
                         navDeepLink { uriPattern = "scanview:{data}" },
                     )
                 ) {
-                    val input = it.arguments?.getString("data")
-                    RequireStarted(walletState, nextUri = "scanview:$input") {
+                    val intent = it.arguments?.getParcelable<Intent>(NavController.KEY_DEEP_LINK_INTENT)
+                    RequireStarted(walletState, nextUri = "scanview:${intent?.data?.toString()}") {
                         ScanDataView(
-                            input = input,
+                            input = intent?.data?.toString()?.substringAfter("scanview:"),
                             onBackClick = { popToHome(navController) },
                             onAuthSchemeInfoClick = { navController.navigate("${Screen.PaymentSettings.route}/true") }
                         )

phoenix-android/src/main/kotlin/fr/acinq/phoenix/android/MainActivity.kt
@@ -117,8 +117,9 @@ class MainActivity : AppCompatActivity() {
         // force the intent flag to single top, in order to avoid [handleDeepLink] finish the current activity.
         // this would otherwise clear the app view model, i.e. loose the state which virtually reboots the app
         // TODO: look into detaching the app state from the activity
-        log.info("receive new_intent=$intent")
+        log.info("receive new_intent with data=${intent?.data}")
         intent?.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
+
         intent?.fixUri()
         this.navController?.handleDeepLink(intent)
     }

phoenix-android/src/main/kotlin/fr/acinq/phoenix/android/payments/ScanDataView.kt
@@ -139,6 +139,9 @@ fun ScanDataView(
                             onPayOnchainClick = {
                                 showPaymentModeDialog = true
                                 postIntent(Scan.Intent.Parse(request = model.uri.copy(paymentRequest = null).write()))
+                            },
+                            onDismiss = {
+                                showPaymentModeDialog = false
                             }
                         )
                     }
@@ -221,10 +224,6 @@ fun ReadDataView(
                 icon = R.drawable.ic_arrow_back,
                 onClick = onBackClick
             )
-        }
-
-        if (model is Scan.Model.BadRequest) {
-            ScanErrorView(model, onFeedbackDismiss)
         }
 
         if (model is Scan.Model.BadRequest) {
@@ -315,24 +314,27 @@ private fun ScanErrorView(
 private fun ChoosePaymentModeDialog(
     onPayOnchainClick: () -> Unit,
     onPayOffchainClick: () -> Unit,
+    onDismiss: () -> Unit,
 ) {
-    Dialog(onDismiss = {}, properties = DialogProperties(dismissOnBackPress = false, dismissOnClickOutside = false), isScrollable = true) {
+    Dialog(onDismiss = onDismiss, properties = DialogProperties(dismissOnBackPress = false, dismissOnClickOutside = true), isScrollable = true, buttons = null) {
         Clickable(onClick = onPayOnchainClick) {
-            Row(modifier = Modifier.padding(24.dp), verticalAlignment = Alignment.CenterVertically) {
+            Row(modifier = Modifier.padding(horizontal = 24.dp, vertical = 16.dp), verticalAlignment = Alignment.CenterVertically) {
                 PhoenixIcon(resourceId = R.drawable.ic_chain)
                 Spacer(Modifier.width(16.dp))
                 Column {
                     Text(text = stringResource(id = R.string.send_paymentmode_onchain), style = MaterialTheme.typography.body2)
+                    Spacer(modifier = Modifier.height(4.dp))
                     Text(text = stringResource(id = R.string.send_paymentmode_onchain_desc), style = MaterialTheme.typography.caption.copy(fontSize = 14.sp))
                 }
             }
         }
         Clickable(onClick = onPayOffchainClick) {
-            Row(modifier = Modifier.padding(24.dp), verticalAlignment = Alignment.CenterVertically) {
+            Row(modifier = Modifier.padding(horizontal = 24.dp, vertical = 18.dp), verticalAlignment = Alignment.CenterVertically) {
                 PhoenixIcon(resourceId = R.drawable.ic_zap)
                 Spacer(Modifier.width(16.dp))
                 Column {
                     Text(text = stringResource(id = R.string.send_paymentmode_lightning), style = MaterialTheme.typography.body2)
+                    Spacer(modifier = Modifier.height(4.dp))
                     Text(text = stringResource(id = R.string.send_paymentmode_lightning_desc), style = MaterialTheme.typography.caption.copy(fontSize = 14.sp))
                 }
             }

commit a99e7f12ee5c1a5c3f56032300f13e90c001a105

(android) Fix parsing of deeplinks from androidx-navigation
This is a temporary workaround for a regression brought in by
f0ae90444a23cc17d6d7407dfe43c0c8d20e62fc which makes the app
parse internal navigation deeplinks and fail with a visible
error message.

Also encode the full "next" URI when passing through startup.

Fixes #487
Fixes #466

phoenix-android/src/main/kotlin/fr/acinq/phoenix/android/AppView.kt
@@ -93,6 +93,8 @@ import fr.acinq.phoenix.data.walletPaymentId
 import fr.acinq.phoenix.legacy.utils.LegacyAppStatus
 import fr.acinq.phoenix.legacy.utils.LegacyPrefsDatastore
 import fr.acinq.phoenix.utils.extensions.id
+import io.ktor.http.decodeURLPart
+import io.ktor.http.encodeURLParameter
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.first
 import org.kodein.memory.util.currentTimestampMillis
@@ -169,8 +171,8 @@ fun AppView(
                         navArgument("next") { type = NavType.StringType; nullable = true }
                     ),
                 ) {
-                    val nextScreenLink = it.arguments?.getString("next")
-                    log.debug { "navigating to startup with next=$nextScreenLink" }
+                    val intent = it.arguments?.getParcelable<Intent>(NavController.KEY_DEEP_LINK_INTENT)
+                    val nextScreenLink = intent?.data?.getQueryParameter("next")?.decodeURLPart()
                     StartupView(
                         appVM = appVM,
                         onShowIntro = { navController.navigate(Screen.Intro.route) },
@@ -178,8 +180,10 @@ fun AppView(
                         onBusinessStarted = {
                             val next = nextScreenLink?.takeUnless { it.isBlank() }?.let { Uri.parse(it) }
                             if (next == null || !navController.graph.hasDeepLink(next)) {
+                                log.debug { "redirecting from startup to home" }
                                 popToHome(navController)
                             } else {
+                                log.debug { "redirecting from startup to $next" }
                                 navController.navigate(next, navOptions = navOptions {
                                     popUpTo(navController.graph.id) { inclusive = true }
                                 })
@@ -240,8 +244,13 @@ fun AppView(
                 ) {
                     val intent = it.arguments?.getParcelable<Intent>(NavController.KEY_DEEP_LINK_INTENT)
                     RequireStarted(walletState, nextUri = "scanview:${intent?.data?.toString()}") {
+                        val input = intent?.data?.toString()?.substringAfter("scanview:")?.takeIf {
+                            // prevents forwarding an internal deeplink intent coming from androidx-navigation framework.
+                            // TODO properly parse deeplinks following f0ae90444a23cc17d6d7407dfe43c0c8d20e62fc
+                            !it.contains("androidx.navigation")
+                        }
                         ScanDataView(
-                            input = intent?.data?.toString()?.substringAfter("scanview:"),
+                            input = input,
                             onBackClick = { popToHome(navController) },
                             onAuthSchemeInfoClick = { navController.navigate("${Screen.PaymentSettings.route}/true") }
                         )
@@ -515,9 +524,8 @@ private fun RequireStarted(
     if (serviceState == null) {
         // do nothing
     } else if (serviceState !is NodeServiceState.Running) {
-        log.debug { "access to screen has been denied (state=${serviceState.name})" }
         val nc = navController
-        nc.navigate("${Screen.Startup.route}?next=$nextUri") {
+        nc.navigate("${Screen.Startup.route}?next=${nextUri?.encodeURLParameter()}") {
             popUpTo(nc.graph.id) { inclusive = true }
         }
     } else {