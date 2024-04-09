0d4e7a8dac3b2f4efb40c3ac2fe4c4caf0c51a57
Dominique Padiou <5765435+dpad85@users.noreply.github.com>
2024-04-09 18:22:43 +0200

    (android) Display remote message notice in home

--- phoenix-android/src/main/kotlin/fr/acinq/phoenix/android/utils/datastore/InternalDataRepository.kt
+++ phoenix-android/src/main/kotlin/fr/acinq/phoenix/android/utils/datastore/InternalDataRepository.kt
@@ -54,6 +54,7 @@ class InternalDataRepository(private val internalData: DataStore<Preferences>) {
         private val CHANNELS_WATCHER_OUTCOME = stringPreferencesKey("CHANNELS_WATCHER_RESULT")
         private val LAST_USED_SWAP_INDEX = intPreferencesKey("LAST_USED_SWAP_INDEX")
         private val INFLIGHT_PAYMENTS_COUNT = intPreferencesKey("INFLIGHT_PAYMENTS_COUNT")
+        private val REMOTE_WALLET_NOTICE_READ_INDEX = intPreferencesKey("REMOTE_WALLET_NOTICE_READ_INDEX")
     }
 
     val log = LoggerFactory.getLogger(this::class.java)
@@ -136,4 +137,6 @@ class InternalDataRepository(private val internalData: DataStore<Preferences>) {
     val getInFlightPaymentsCount: Flow<Int> = safeData.map { it[INFLIGHT_PAYMENTS_COUNT] ?: 0 }
     suspend fun saveInFlightPaymentsCount(count: Int) = internalData.edit { it[INFLIGHT_PAYMENTS_COUNT] = count }
 
+    val getLastReadWalletNoticeIndex: Flow<Int> = safeData.map { it[REMOTE_WALLET_NOTICE_READ_INDEX] ?: -1 }
+    suspend fun saveLastReadWalletNoticeIndex(index: Int) = internalData.edit { it[REMOTE_WALLET_NOTICE_READ_INDEX] = index }
 }
