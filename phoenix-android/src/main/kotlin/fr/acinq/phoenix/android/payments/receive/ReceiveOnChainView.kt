c1f6de85d4ec2a0bae69dce58d4ec24139dbbe3e
Dominique Padiou <5765435+dpad85@users.noreply.github.com>
2024-04-09 15:32:52 +0200

    (android) Remove fee message in on-chain receive screen

--- phoenix-android/src/main/kotlin/fr/acinq/phoenix/android/payments/receive/ReceiveOnChainView.kt
+++ phoenix-android/src/main/kotlin/fr/acinq/phoenix/android/payments/receive/ReceiveOnChainView.kt
@@ -76,14 +76,6 @@ fun BitcoinAddressView(
             HSeparator(width = 50.dp)
             Spacer(modifier = Modifier.height(16.dp))
             QRCodeDetail(label = stringResource(id = R.string.receive_bitcoin_address_label), value = state.currentAddress)
-
-            Spacer(modifier = Modifier.height(24.dp))
-            InfoMessage(
-                header = stringResource(id = R.string.receive_onchain_fees_title),
-                details = stringResource(id = R.string.receive_onchain_fees_body1),
-                detailsStyle = MaterialTheme.typography.subtitle2,
-                alignment = Alignment.CenterHorizontally
-            )
         }
         is BitcoinAddressState.Error -> {
             ErrorMessage(
