e1bed63f1f02da4306f98d82576fa522f3935376
Dominique Padiou <5765435+dpad85@users.noreply.github.com>
2024-04-09 19:37:19 +0200

    (android) Release v2.2.2 (code 78)

--- phoenix-android/build.gradle.kts
+++ phoenix-android/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 78
-        versionName = gitCommitHash()
+        versionName = "2.2.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         resourceConfigurations.addAll(listOf("en", "fr", "de", "es", "b+es+419", "cs", "pt-rBR", "sk", "vi"))
     }
