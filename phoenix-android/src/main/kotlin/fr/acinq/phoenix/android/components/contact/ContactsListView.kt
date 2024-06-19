0539495d0c9d2562bb513bf554ef71d657feca18
Dominique Padiou <5765435+dpad85@users.noreply.github.com>
2024-06-19 11:03:22 +0200

    (android) Fix contacts list UI

--- phoenix-android/src/main/kotlin/fr/acinq/phoenix/android/components/contact/ContactsListView.kt
+++ phoenix-android/src/main/kotlin/fr/acinq/phoenix/android/components/contact/ContactsListView.kt
@@ -16,9 +16,14 @@
 
 package fr.acinq.phoenix.android.components.contact
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -26,14 +31,15 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import fr.acinq.lightning.wire.OfferTypes
 import fr.acinq.phoenix.android.R
 import fr.acinq.phoenix.android.business
 import fr.acinq.phoenix.android.components.Button
 import fr.acinq.phoenix.android.components.ItemCard
+import fr.acinq.phoenix.android.components.ProgressView
 import fr.acinq.phoenix.data.ContactInfo
 
 @Composable
@@ -44,7 +50,7 @@ fun ContactsListView(
     val contactsState = business.contactsManager.contactsList.collectAsState(null)
     contactsState.value?.let { contacts ->
         if (contacts.isEmpty()) {
-            Text(text = "No contacts yet...")
+            Text(text = "No contacts yet...", modifier = Modifier.padding(16.dp), style = MaterialTheme.typography.caption)
         } else {
             val listState = rememberLazyListState()
             LazyColumn(state = listState) {
@@ -57,19 +63,26 @@ fun ContactsListView(
                             contact.offers.firstOrNull()?.let { onExecuteOffer(it) }
                                 ?: run { onContactClick(contact) }
                         },
-                        internalPadding = PaddingValues(horizontal = 24.dp, vertical = 16.dp)
                     ) {
-                        Text(
-                            text = contact.name,
-                            modifier = Modifier.fillMaxWidth(),
-                        )
-                        Button(
-                            icon = R.drawable.ic_edit,
-                            onClick = { onContactClick(contact) }
-                        )
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                        ) {
+                            Spacer(modifier = Modifier.width(12.dp))
+                            ContactPhotoView(image = contact.photo?.toByteArray(), name = contact.name, onChange = {}, imageSize = 32.dp, borderSize = 0.dp)
+                            Spacer(modifier = Modifier.width(8.dp))
+                            Text(
+                                text = contact.name,
+                                modifier = Modifier.weight(1f),
+                            )
+                            Button(
+                                icon = R.drawable.ic_edit,
+                                onClick = { onContactClick(contact) },
+                                padding = PaddingValues(12.dp)
+                            )
+                        }
                     }
                 }
             }
         }
-    } ?: Text(text = "Fetching contacts...")
+    } ?: ProgressView(text = "Fetching contacts...")
 }
