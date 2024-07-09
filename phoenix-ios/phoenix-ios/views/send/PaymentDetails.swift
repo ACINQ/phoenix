6ea6345ac92127702b1b1830939b27be6d53d682
Robbie Hanson <304604+robbiehanson@users.noreply.github.com>
2024-07-09 14:53:18 -0500

    (ios) Fixing grid layout. Should now be visually centered.

--- phoenix-ios/phoenix-ios/views/send/PaymentDetails.swift
+++ phoenix-ios/phoenix-ios/views/send/PaymentDetails.swift
@@ -8,11 +8,19 @@ fileprivate var log = LoggerFactory.shared.logger(filename, .trace)
 fileprivate var log = LoggerFactory.shared.logger(filename, .warning)
 #endif
 
+fileprivate let grid_horizontalSpacing: CGFloat = 8
+fileprivate let grid_verticalSpacing: CGFloat = 12
+
 struct PaymentDetails: View {
 	
 	let parent: ValidateView
 	
-	private let valueFont: Font = .subheadline
+	enum GridWidth: Preference {}
+	let gridWidthReader = GeometryPreferenceReader(
+		key: AppendValue<GridWidth>.self,
+		value: { [$0.size.width] }
+	)
+	@State var gridWidth: CGFloat? = nil
 	
 	@ViewBuilder
 	var body: some View {
@@ -34,11 +42,24 @@ struct PaymentDetails: View {
 	@available(iOS 16.0, *)
 	func details_ios16() -> some View {
 		
-		Grid(horizontalSpacing: 8, verticalSpacing: 12) {
+		HStack(alignment: VerticalAlignment.center, spacing: 0) {
+			Spacer(minLength: 0)
+			grid()
+			Spacer(minLength: 0)
+		}
+		.read(gridWidthReader)
+		.assignMaxPreference(for: gridWidthReader.key, to: $gridWidth)
+	}
+
+	@ViewBuilder
+	@available(iOS 16.0, *)
+	func grid() -> some View {
+		
+		Grid(horizontalSpacing: grid_horizontalSpacing, verticalSpacing: grid_verticalSpacing) {
 			
 			if let model = parent.mvi.model as? Scan.Model_OnChainFlow {
 				gridRows_onChain(model)
-			} else {
+			} else if requestDescription() != nil {
 				gridRows_description()
 			}
 			if let model = parent.mvi.model as? Scan.Model_OfferFlow {
@@ -52,23 +73,22 @@ struct PaymentDetails: View {
 	
 	@ViewBuilder
 	@available(iOS 16.0, *)
-	func gridRows_onChain(_ model: Scan.Model_OnChainFlow) -> some View {
+	func gridRows_onChain(
+		_ model: Scan.Model_OnChainFlow
+	) -> some View {
 		
 		let message = bitcoinUriMessage()
 		if message != nil {
-			GridRow(alignment: VerticalAlignment.firstTextBaseline) {
+			GridRowWrapper(gridWidth: gridWidth) {
 				titleColumn("Type")
-				
+			} valueColumn: {
 				Text("On-Chain Payment")
-					.font(valueFont)
-					.gridCellColumns(2)
-					.gridCellAnchor(.leading)
-				
-			} // </GridRow>
+			} // </GridRowWrapper>
 		}
-		GridRow(alignment: VerticalAlignment.firstTextBaseline) {
+		
+		GridRowWrapper(gridWidth: gridWidth) {
 			titleColumn("Desc")
-			
+		} valueColumn: {
 			Group {
 				if let message {
 					Text(message)
@@ -76,21 +96,15 @@ struct PaymentDetails: View {
 					Text("On-Chain Payment")
 				}
 			}
-			.font(valueFont)
-			.gridCellColumns(2)
-			.gridCellAnchor(.leading)
-			
-		} // </GridRow>
-		GridRow(alignment: VerticalAlignment.firstTextBaseline) {
+		} // </GridRowWrapper>
+		
+		GridRowWrapper(gridWidth: gridWidth) {
 			titleColumn("Send To")
-			
+		} valueColumn: {
 			let btcAddr = model.uri.address
 			Text(btcAddr)
 				.lineLimit(2)
 				.truncationMode(.middle)
-				.font(valueFont)
-				.gridCellColumns(2)
-				.gridCellAnchor(.leading)
 				.contextMenu {
 					Button {
 						UIPasteboard.general.string = btcAddr
@@ -98,32 +112,31 @@ struct PaymentDetails: View {
 						Text("Copy")
 					}
 				}
-		} // </GridRow>
+		} // </GridRowWrapper>
 	}
 	
 	@ViewBuilder
 	@available(iOS 16.0, *)
 	func gridRows_description() -> some View {
 		
-		GridRow(alignment: VerticalAlignment.firstTextBaseline) {
+		GridRowWrapper(gridWidth: gridWidth) {
 			titleColumn("Desc")
-			
-			Text(requestDescription())
+		} valueColumn: {
+			Text(requestDescription() ?? "")
 				.lineLimit(3)
 				.truncationMode(.tail)
-				.font(valueFont)
-				.gridCellColumns(2)
-				.gridCellAnchor(.leading)
-			
-		} // </GridRow>
+		} // </GridRowWrapper>
 	}
 	
 	@ViewBuilder
 	@available(iOS 16.0, *)
-	func gridRows_offer(_ model: Scan.Model_OfferFlow) -> some View {
+	func gridRows_offer(
+		_ model: Scan.Model_OfferFlow
+	) -> some View {
 		
-		GridRow(alignment: VerticalAlignment.firstTextBaseline) {
+		GridRowWrapper(gridWidth: gridWidth) {
 			titleColumn("Send To")
+		} valueColumn: {
 				
 			if CONTACTS_ENABLED, let contact = parent.contact {
 				
@@ -146,13 +159,10 @@ struct PaymentDetails: View {
 					.clipShape(Circle())
 					
 					Text(contact.name)
-				}
-				.font(valueFont)
+				} // <HStack>
 				.onTapGesture {
 					parent.showManageContactSheet()
 				}
-				.gridCellColumns(2)
-				.gridCellAnchor(.leading)
 				
 			} else {
 				
@@ -179,15 +189,12 @@ struct PaymentDetails: View {
 						}
 					}
 				} // </VStack>
-				.font(valueFont)
-				.gridCellColumns(2)
-				.gridCellAnchor(.leading)
 			}
-		} // </GridRow>
+		} // </GridRowWrapper>
 		
-		GridRow(alignment: VerticalAlignment.firstTextBaseline) {
+		GridRowWrapper(gridWidth: gridWidth) {
 			titleColumn("Message")
-			
+		} valueColumn: {
 			Group {
 				let comment = parent.comment
 				if comment.isEmpty {
@@ -209,16 +216,14 @@ struct PaymentDetails: View {
 					}
 				}
 			} // </Group>
-			.font(valueFont)
-			.gridCellColumns(2)
-			.gridCellAnchor(.leading)
-			
-		} // </GridRow>
+		} // </GridRowWrapper>
 	}
 	
 	@ViewBuilder
 	@available(iOS 16.0, *)
-	func gridRows_paymentSummary(_ info: PaymentSummaryStrings) -> some View {
+	func gridRows_paymentSummary(
+		_ info: PaymentSummaryStrings
+	) -> some View {
 		
 		let titleColor   = info.isEmpty ? Color.clear : Color.secondary
 		let bitcoinColor = info.isEmpty ? Color.clear : Color.primary
@@ -226,96 +231,77 @@ struct PaymentDetails: View {
 		let percentColor = info.isEmpty ? Color.clear : Color.secondary
 		
 		if info.hasTip {
-			GridRow(alignment: VerticalAlignment.firstTextBaseline) {
+			GridRowWrapper(gridWidth: gridWidth) {
 				titleColumn("tip", titleColor)
-				
+			} valueColumn: {
 				VStack(alignment: HorizontalAlignment.leading, spacing: 4) {
 					Text(verbatim: info.bitcoin_tip.string)
 						.foregroundColor(bitcoinColor)
 					Text(verbatim: " ≈\(info.fiat_tip.string)")
 						.foregroundColor(fiatColor)
+					Text(verbatim: info.percent_tip)
+						.foregroundColor(percentColor)
 				}
-				.font(valueFont)
-				.gridCellAnchor(.leading)
-				
-				Text(verbatim: info.percent_tip)
-					.font(valueFont)
-					.foregroundColor(percentColor)
-					.gridCellAnchor(.leading)
-				
-			} // </GridRow>
+			} // </GridRowWrapper>
 			.accessibilityLabel(accessibilityLabel_tipAmount(info))
 			
 		} // </hasTip>
 		
 		if info.hasLightningFee || info.isEmpty {
-			GridRow(alignment: VerticalAlignment.firstTextBaseline) {
+			GridRowWrapper(gridWidth: gridWidth) {
 				titleColumn("lightning fee", titleColor)
-				
+			} valueColumn: {
 				VStack(alignment: HorizontalAlignment.leading, spacing: 4) {
 					Text(verbatim: info.bitcoin_lightningFee.string)
 						.foregroundColor(bitcoinColor)
 					Text(verbatim: " ≈\(info.fiat_lightningFee.string)")
 						.foregroundColor(fiatColor)
+					Text(verbatim: info.percent_lightningFee)
+						.foregroundColor(percentColor)
 				}
-				.font(valueFont)
-				.gridCellAnchor(.leading)
-				
-				Text(verbatim: info.percent_lightningFee)
-					.font(valueFont)
-					.foregroundColor(percentColor)
-					.gridCellAnchor(.leading)
-				
-			} // </GridRow>
+			} // </GridRowWrapper>
 			.accessibilityLabel(accessibilityLabel_lightningFeeAmount(info))
 			
 		} // </hasLightningFee>
 		
 		if info.hasMinerFee {
-			GridRow(alignment: VerticalAlignment.firstTextBaseline) {
+			GridRowWrapper(gridWidth: gridWidth) {
 				titleColumn("miner fee", titleColor)
-				
-				VStack(alignment: HorizontalAlignment.leading, spacing: 4) {
-					Text(verbatim: info.bitcoin_minerFee.string)
-						.foregroundColor(bitcoinColor)
-					Text(verbatim: " ≈\(info.fiat_minerFee.string)")
-						.foregroundColor(fiatColor)
-				}
-				.font(valueFont)
-				.gridCellAnchor(.leading)
-				
-				VStack(alignment: HorizontalAlignment.leading, spacing: 4) {
-					Text(verbatim: info.percent_minerFee)
-						.font(valueFont)
-						.foregroundColor(percentColor)
+			} valueColumn: {
+				HStack(alignment: VerticalAlignment.firstTextBaseline, spacing: 4) {
+					
+					VStack(alignment: HorizontalAlignment.leading, spacing: 4) {
+						Text(verbatim: info.bitcoin_minerFee.string)
+							.foregroundColor(bitcoinColor)
+						Text(verbatim: " ≈\(info.fiat_minerFee.string)")
+							.foregroundColor(fiatColor)
+						Text(verbatim: info.percent_minerFee)
+							.foregroundColor(percentColor)
+					} // </VStack>
 					
 					Button {
 						parent.showMinerFeeSheet()
 					} label: {
 						Image(systemName: "square.and.pencil").font(.body)
 					}
-				}
-				.gridCellAnchor(.leading)
-				
-			} // </GridRow>
+					
+				} // </HStack>
+			} // </GridRowWrapper>
 			.accessibilityLabel(accessibilityLabel_minerFeeAmount(info))
 			
 		} // </hasMinerFee>
 		
 		if info.hasTip || info.hasLightningFee || info.hasMinerFee || info.isEmpty {
-			GridRow(alignment: VerticalAlignment.firstTextBaseline) {
+			GridRowWrapper(gridWidth: gridWidth) {
 				titleColumn("total", titleColor)
-				
+			} valueColumn: {
 				VStack(alignment: HorizontalAlignment.leading, spacing: 4) {
 					Text(verbatim: info.bitcoin_total.string)
 						.foregroundColor(bitcoinColor)
 					Text(verbatim: " ≈\(info.fiat_total.string)")
 						.foregroundColor(fiatColor)
 				}
-				.font(valueFont)
-				.gridCellAnchor(.leading)
-				
-			} // </GridRow>
+			} // </GridRowWrapper>
 			.accessibilityLabel(accessibilityLabel_totalAmount(info))
 			
 		} // </total>
@@ -323,14 +309,14 @@ struct PaymentDetails: View {
 	
 	@ViewBuilder
 	@available(iOS 16.0, *)
-	func titleColumn(_ title: LocalizedStringKey, _ color: Color = Color.secondary) -> some View {
+	func titleColumn(
+		_ title: LocalizedStringKey,
+		_ color: Color = Color.secondary
+	) -> some View {
 		
 		Text(title)
 			.textCase(.uppercase)
-			.font(.subheadline)
 			.foregroundColor(color)
-			.gridCellAnchor(.trailing)
-		//	.frame(minWidth: 125, alignment: .trailing) // Todo: Replace hard-coded value
 	}
 	
 	// --------------------------------------------------
@@ -349,23 +335,32 @@ struct PaymentDetails: View {
 		return trimmedMessage.isEmpty ? nil : trimmedMessage
 	}
 	
-	func requestDescription() -> String {
+	func requestDescription() -> String? {
 		
-		var desc: String? = nil
-		if let invoice = parent.bolt11Invoice() {
-			desc = invoice.desc_()
+		let sanitize = { (input: String?) -> String? in
 			
-		} else if let offer = parent.bolt12Offer() {
-			desc = offer.description_
-			
-		} else if let lnurlPay = parent.lnurlPay() {
-			desc = lnurlPay.metadata.plainText
-			
-		} else if let lnurlWithdraw = parent.lnurlWithdraw() {
-			desc = lnurlWithdraw.defaultDescription
+			if let trimmedInput = input?.trimmingCharacters(in: .whitespacesAndNewlines) {
+				if !trimmedInput.isEmpty {
+					return trimmedInput
+				}
+			}
+			return nil
 		}
 		
-		return desc ?? String(localized: "No description")
+		if let invoice = parent.bolt11Invoice() {
+			return sanitize(invoice.desc_())
+			
+		} else if let offer = parent.bolt12Offer() {
+			return sanitize(offer.description_)
+			
+		} else if let lnurlPay = parent.lnurlPay() {
+			return sanitize(lnurlPay.metadata.plainText)
+			
+		} else if let lnurlWithdraw = parent.lnurlWithdraw() {
+			return sanitize(lnurlWithdraw.defaultDescription)
+		}
+		
+		return nil
 	}
 	
 	func accessibilityLabel_tipAmount(_ info: PaymentSummaryStrings) -> String {
@@ -413,6 +408,47 @@ struct PaymentDetails: View {
 			"Total amount: \(amountBitcoin), ≈\(amountFiat)",
 			comment: "VoiceOver label: PaymentSummaryView"
 		)
+	}
+}
+
+@available(iOS 16.0, *)
+struct GridRowWrapper<KeyColumn: View, ValueColumn: View>: View {
+	
+	let gridWidth: CGFloat?
+	let keyColumn: KeyColumn
+	let valueColumn: ValueColumn
+	
+	init(
+		gridWidth: CGFloat?,
+		@ViewBuilder keyColumn keyColumnBuilder: () -> KeyColumn,
+		@ViewBuilder valueColumn valueColumnBuilder: () -> ValueColumn
+	) {
+		self.gridWidth = gridWidth
+		self.keyColumn = keyColumnBuilder()
+		self.valueColumn = valueColumnBuilder()
+	}
+	
+	@ViewBuilder
+	var body: some View {
+		
+		GridRow(alignment: VerticalAlignment.firstTextBaseline) {
+			keyColumn
+				.font(.subheadline)
+				.frame(maxWidth: columnWidth, alignment: Alignment.topTrailing)
+				.gridCellAnchor(.topTrailing)
+			
+			valueColumn
+				.font(.subheadline)
+				.frame(minWidth: columnWidth, alignment: Alignment.leading)
+				.gridCellAnchor(.leading)
+		}
+	}
+	
+	var columnWidth: CGFloat? {
+		guard let gridWidth else {
+			return nil
+		}
+		return (gridWidth / 2.0) - (grid_horizontalSpacing / 2.0)
 	}
 }
 
