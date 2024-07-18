d8712b45b5ce43a9afe5edf02de375b2d2d4050e
Robbie Hanson <304604+robbiehanson@users.noreply.github.com>
2024-07-18 11:47:32 -0500

    (ios) Addressing performance issue in ContactsList. Adding ability to edit/delete items from ContactsList.

--- phoenix-ios/phoenix-ios/views/widgets/ContactPhoto.swift
+++ phoenix-ios/phoenix-ios/views/widgets/ContactPhoto.swift
@@ -1,18 +1,51 @@
 import SwiftUI
+import PhoenixShared
 
 fileprivate let filename = "ContactPhoto"
 #if DEBUG && true
-fileprivate var log = LoggerFactory.shared.logger(filename, .trace)
+fileprivate var log = LoggerFactory.shared.logger(filename, .warning)
 #else
 fileprivate var log = LoggerFactory.shared.logger(filename, .warning)
 #endif
 
 struct ContactPhoto: View {
 	
-	@State var fileName: String?
-	@State var size: CGFloat
+	let fileName: String?
+	let size: CGFloat
+	
+	@ViewBuilder
+	var body: some View {
+		
+		_ContactPhoto(fileName: fileName, size: size)
+			.id("\(fileName ?? "<nil>")@\(size)") // <- required
+		
+		// Due to "structural identity" in SwiftUI:
+		// - even when `fileName` or `size` changes, it's still considered to be the "same" view
+		// - which means we don't receive a notification via `onAppear`
+		// - nor do our `.task` items re-fire
+		//
+		// In other words:
+		// - zero notifications
+		// - only a silent re-run of our ViewBuilder `body`
+		//
+		// To get around this, we use `.id` to force "explicit identity".
+		// So when `fileName` or `size` changes, it will be a new instance of `_ContactPhoto`.
+	}
+}
+
+fileprivate struct _ContactPhoto: View {
+	
+	let fileName: String?
+	let size: CGFloat
 	
 	@State private var bgLoadedImage: UIImage? = nil
+	
+	init(fileName: String?, size: CGFloat) {
+		self.fileName = fileName
+		self.size = size
+		
+		log.trace("[private] init(): \(fileName ?? "<nil>")")
+	}
 	
 	@ViewBuilder
 	var body: some View {
@@ -31,33 +64,36 @@ struct ContactPhoto: View {
 		}
 		.frame(width: size, height: size)
 		.clipShape(Circle())
-		.onAppear {
-			log.trace("onAppear()")
-		}
 		.task {
 			await loadImage()
-		}
-		.onChange(of: fileName) { _ in
-			fileNameChanged()
 		}
 	}
 	
 	func loadImage() async {
-		log.trace("loadImage(): \(fileName ?? "<nil>")")
+		log.trace("[private] loadImage(): \(fileName ?? "<nil>")")
 		
-		guard let photoFileName = fileName else {
+		guard let fileName else {
 			return
 		}
 		
-		let filePath = PhotosManager.shared.filePathForPhoto(fileName: photoFileName)
-		if let img = UIImage(contentsOfFile: filePath) {
-			bgLoadedImage = img
+		let fileUrl = PhotosManager.shared.urlForPhoto(fileName: fileName)
+		DispatchQueue.global(qos: .userInteractive).async {
+			do {
+				let data = try Data(contentsOf: fileUrl, options: [.mappedIfSafe, .uncached])
+				guard let fullSizePhoto = UIImage(data: data) else {
+					return
+				}
+				
+				let cgsize = CGSize(width: size, height: size)
+				let scaledPhoto = UIGraphicsImageRenderer(size: cgsize).image { _ in
+					fullSizePhoto.draw(in: CGRect(origin: .zero, size: cgsize))
+				}
+				
+				bgLoadedImage = scaledPhoto
+				
+			} catch {
+				log.warning("loadImage: error: \(error)")
+			}
 		}
-	}
-	
-	func fileNameChanged() {
-		log.trace("fileNameChanged(): \(fileName ?? "<nil>")")
-		
-		// Todo...
 	}
 }
