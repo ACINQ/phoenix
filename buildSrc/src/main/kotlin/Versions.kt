87b35c5c869d9350c4e8a6c822b7efd674d60b91
Dominique Padiou <5765435+dpad85@users.noreply.github.com>
2024-09-18 13:25:23 +0200

    Upgrade to kotlin v2.0.10
    Also upgraded:
    - ktor to 2.3.12
    - kotlinx.serialisation to 1.7.2 (transitive)
    - kotlinx.coroutine to 1.9.0 (transitive)
    - kermit to 2.0.4 (transitive)
    - skie to 0.8.4
    - sqldelight to 2.0.2

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,10 +3,10 @@ object Versions {
     const val secp256k1 = "0.14.0"
     const val torMobile = "0.2.0"
 
-    const val kotlin = "1.9.22"
+    const val kotlin = "2.0.10"
 
-    const val ktor = "2.3.7"
-    const val sqlDelight = "2.0.1"
+    const val ktor = "2.3.12"
+    const val sqlDelight = "2.0.2"
 
     const val slf4j = "1.7.30"
     const val junit = "4.13"
