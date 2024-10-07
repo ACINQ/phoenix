81e41045f168cf970a64b1d651addc8609fcaee5
Dominique Padiou <5765435+dpad85@users.noreply.github.com>
2024-10-07 17:43:45 +0200

    (android) Increment build code (92)

--- phoenix-android/build.gradle.kts
+++ phoenix-android/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         applicationId = "fr.acinq.phoenix.testnet"
         minSdk = 26
         targetSdk = 34
-        versionCode = 91
+        versionCode = 92
         versionName = gitCommitHash()
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         resourceConfigurations.addAll(listOf("en", "fr", "de", "es", "b+es+419", "cs", "pt-rBR", "sk", "vi", "sw"))
