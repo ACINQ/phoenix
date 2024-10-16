6ead4c47d578aa801ffaea7df61fc09a98d52288
Dominique Padiou <5765435+dpad85@users.noreply.github.com>
2024-10-16 14:20:35 +0200

    (android) Increment build code (94)

--- phoenix-legacy/build.gradle.kts
+++ phoenix-legacy/build.gradle.kts
@@ -30,7 +30,7 @@ android {
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
   buildTypes {
-    val libCode = 93
+    val libCode = 94
     getByName("debug") {
       resValue("string", "CHAIN", chain)
       buildConfigField("String", "CHAIN", chain)
