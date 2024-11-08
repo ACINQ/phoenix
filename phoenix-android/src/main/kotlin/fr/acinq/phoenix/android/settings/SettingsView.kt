3f9d14f54321ce66080b2a0a05a075d4a5f9034d
Crazy Unicorn <30509883+Crazy-Unicooorn@users.noreply.github.com>
2024-11-08 19:03:45 +0100

    (android) Fix settings text of experimental features (#647)

--- phoenix-android/src/main/kotlin/fr/acinq/phoenix/android/settings/SettingsView.kt
+++ phoenix-android/src/main/kotlin/fr/acinq/phoenix/android/settings/SettingsView.kt
@@ -109,7 +109,7 @@ fun SettingsView(
         Card {
             MenuButton(text = stringResource(R.string.settings_wallet_info), icon = R.drawable.ic_box, onClick = { nc.navigate(Screen.WalletInfo) })
             MenuButton(text = stringResource(R.string.settings_list_channels), icon = R.drawable.ic_zap, onClick = { nc.navigate(Screen.Channels) })
-            MenuButton(text = "Experimental features", icon = R.drawable.ic_experimental, onClick = { nc.navigate(Screen.Experimental) })
+            MenuButton(text = stringResource(R.string.experimental_title), icon = R.drawable.ic_experimental, onClick = { nc.navigate(Screen.Experimental) })
             MenuButton(text = stringResource(R.string.settings_logs), icon = R.drawable.ic_text, onClick = { nc.navigate(Screen.Logs) })
         }
         // -- advanced
