cc34528c9859984bbc2bc17a056e5db40301c27e
Dominique Padiou <5765435+dpad85@users.noreply.github.com>
2024-11-18 16:32:14 +0100

    (android) Migrate to compose compiler gradle plugin
    Also removed Tor helper method.

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -18,7 +18,6 @@ object Versions {
         const val lifecycle = "2.6.0"
         const val datastore = "1.0.0"
         const val compose = "1.6.8"
-        const val composeCompiler = "1.5.8"
         const val navCompose = "2.6.0"
         const val accompanist = "0.30.1"
         const val composeConstraintLayout = "1.1.0-alpha09"
