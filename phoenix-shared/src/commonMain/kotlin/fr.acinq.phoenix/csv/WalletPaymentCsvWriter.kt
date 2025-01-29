ea47e9d635ea1de8e2a975830d48e27f3b929d78
pm47 <pm.padiou@gmail.com>
2025-01-29 17:24:45 +0100

    fixup in csv for incoming ln payment amount

--- phoenix-shared/src/commonMain/kotlin/fr.acinq.phoenix/csv/WalletPaymentCsvWriter.kt
+++ phoenix-shared/src/commonMain/kotlin/fr.acinq.phoenix/csv/WalletPaymentCsvWriter.kt
@@ -143,7 +143,7 @@ class WalletPaymentCsvWriter(val configuration: Configuration) : CsvWriter() {
         val details: Details? = when (payment) {
             is LightningIncomingPayment -> Details(
                 type = Type.lightning_received,
-                amount = payment.amount,
+                amount = payment.parts.filterIsInstance<LightningIncomingPayment.Part.Htlc>().map { it.amountReceived }.sum(),
                 feeCredit = payment.parts.filterIsInstance<LightningIncomingPayment.Part.FeeCredit>().map { it.amountReceived }.sum() - (payment.liquidityPurchaseDetails?.feeCreditUsed ?: 0.msat),
                 miningFee = payment.liquidityPurchaseDetails?.miningFee ?: 0.sat,
                 serviceFee = payment.liquidityPurchaseDetails?.purchase?.fees?.serviceFee?.toMilliSatoshi() ?: 0.msat,
