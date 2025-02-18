e50f888ebe234c46fb9a008f991dc6dbd7f9675b
Robbie Hanson <304604+robbiehanson@users.noreply.github.com>
2025-02-18 09:55:52 -0500

    WIP 4

--- phoenix-ios/phoenix-ios/nfc/DnaCommunicator/Configuration/Ndef.swift
+++ phoenix-ios/phoenix-ios/nfc/DnaCommunicator/Configuration/Ndef.swift
@@ -38,37 +38,62 @@ enum NdefHeaderType: UInt8 {
 
 public class Ndef {
 	
-	static let HEADER_SIZE = 7
+	static let URL_HEADER_SIZE = 7
+	static let TEXT_HEADER_SIZE = 9
 	
 	class func ndefDataForUrl(_ url: URL) -> [UInt8] {
 		
 		// See pgs. 30-31 of AN12196
 		
+		// NDEF File Format:
+		//
+		// Field        | Length     | Description
+		// ---------------------------------------------------------------
+		// NLEN         | 2 bytes    | Length of the NDEF message in big-endian format.
+		// NDEF Message | NLEN bytes | NDEF message. See NFC Data Exchange Format (NDEF).
+		//
+		// https://docs.nordicsemi.com/bundle/ncs-latest/page/nrfxlib/nfc/doc/type_4_tag.html#t4t-format
+		//
+		var fileHeader: [UInt8] = [
+			0x00, // Placeholder for NLEN
+			0x00, // Placeholder for NLEN
+		]
+		
+		// NDEF Message header:
+		
 		let flags: NdefHeaderFlags = [.MB, .ME, .SR, .TNF_WELL_KNOWN]
 		let type = NdefHeaderType.URL
 		
-		let header: [UInt8] = [
-			0x00,           // Placeholder for data size (two bytes MSB)
-			0x00,
+		var messageHeader: [UInt8] = [
 			flags.rawValue, // NDEF header flags
-			0x01,           // Length of "type" field
-			0x00,           // URL size placeholder
-			type.rawValue,  // This will be a URL record
-			0x00            // Just the URI (no prepended protocol)
+			0x01,           // Type length
+			0x00,           // URL length placeholder
+			type.rawValue   // Well-known type: URL
+		]
+		
+		// Header for: Well-known-type(URL)
+		//
+		// Note: If you have a long URL that doesn't fit, you can change the typeHeader here.
+		// For example, if you specify 0x02 for the typeHeader, it means:
+		// - prepend `https://www.` to the URL content, saving a few bytes.
+		//
+		let typeHeader: [UInt8] = [
+			0x00 // Just the URI (no prepended protocol)
 		]
 		
 		let urlData = url.absoluteString.data(using: .utf8) ?? Data()
 		var urlBytes = Helper.bytesFromData(data: urlData)
 		
-		let maxUrlSize = 255 - header.count
+		let maxFileSize = 256 // As per NTAG 424 spcification for File #2
+		let maxUrlSize = maxFileSize - (fileHeader.count + messageHeader.count + typeHeader.count)
 		if urlBytes.count > maxUrlSize {
 			urlBytes = Array(urlBytes[0..<maxUrlSize])
 		}
 		
-		var result: [UInt8] = header + urlBytes
-		result[1] = UInt8(result.count - 2)   // Length of everything that isn't the length
-		result[4] = UInt8(urlBytes.count + 1) // Everything after type field
+		fileHeader[1] = UInt8(messageHeader.count + typeHeader.count + urlBytes.count)
+		messageHeader[2] = UInt8(typeHeader.count + urlBytes.count)
 		
+		let result: [UInt8] = fileHeader + messageHeader + typeHeader + urlBytes
 		return result
 	}
 	
@@ -76,31 +101,72 @@ public class Ndef {
 		
 		// See pgs. 30-31 of AN12196
 		
+		// NDEF File Format:
+		//
+		// Field        | Length     | Description
+		// ---------------------------------------------------------------
+		// NLEN         | 2 bytes    | Length of the NDEF message in big-endian format.
+		// NDEF Message | NLEN bytes | NDEF message. See NFC Data Exchange Format (NDEF).
+		//
+		// https://docs.nordicsemi.com/bundle/ncs-latest/page/nrfxlib/nfc/doc/type_4_tag.html#t4t-format
+		//
+		var fileHeader: [UInt8] = [
+			0x00, // Placeholder for NLEN
+			0x00  // Placeholder for NLEN
+		]
+		
+		// NDEF Message header:
+		
 		let flags: NdefHeaderFlags = [.MB, .ME, .SR, .TNF_WELL_KNOWN]
 		let type = NdefHeaderType.TEXT
 		
-		let header: [UInt8] = [
-			0x00,           // Placeholder for data size (two bytes MSB)
-			0x00,
+		var messageHeader: [UInt8] = [
 			flags.rawValue, // NDEF header flags
-			0x01,           // Length of "type" field
-			0x00,           // URL size placeholder
-			type.rawValue,  // This will be a TEXT record
-			0x00            // Just the text (no prepended protocol)
+			0x01,           // Type length
+			0x00,           // Text length placeholder
+			type.rawValue   // Well-known type: TEXT
+		]
+		
+		// Header for: Well-known-type(TEXT)
+		//
+		// RTD TEXT specification:
+		//
+		// Byte 0 bit pattern:
+		//
+		// |     7    |    6     |   5, 4, 3, 2, 1, 0   |
+		// ----------------------------------------------
+		// | UTF 8/16 | Reserved | Language code length |
+		//
+		// UTF-8  => 0
+		// UTF-16 => 1
+		//
+		// Reserved => must be 0
+		//
+		// Language code should use ISO/IANA language code.
+		// We will use "en" - although for our use case it will be ignored.
+		//
+		// Thus our bit pattern is:
+		// 0b00000010 = 0x02
+		//
+		let typeHeader: [UInt8] = [
+			0x02, // UTF-8; langCode.length = 2
+			0x65, // 'e'
+			0x6e  // 'n'
 		]
 		
 		let textData = text.data(using: .utf8) ?? Data()
 		var textBytes = Helper.bytesFromData(data: textData)
 		
-		let maxTextSize = 255 - header.count
+		let maxFileSize = 256 // As per NTAG 424 spcification for File #2
+		let maxTextSize = maxFileSize - (fileHeader.count + messageHeader.count + typeHeader.count)
 		if textBytes.count > maxTextSize {
 			textBytes = Array(textBytes[0..<maxTextSize])
 		}
 		
-		var result: [UInt8] = header + textBytes
-		result[1] = UInt8(result.count - 2)    // Length of everything that isn't the length
-		result[4] = UInt8(textBytes.count + 1) // Everything after type field
+		fileHeader[1] = UInt8(messageHeader.count + typeHeader.count + textBytes.count)
+		messageHeader[2] = UInt8(typeHeader.count + textBytes.count)
 		
+		let result: [UInt8] = fileHeader + messageHeader + typeHeader + textBytes
 		return result
 	}
 	
@@ -176,8 +242,8 @@ public class Ndef {
 			let offset2 = urlUtf8.distance(from: urlUtf8.startIndex, to: range2.upperBound)
 			
 			self.value = Either.Left(resolvedUrl)
-			self.piccDataOffset = offset1 + Ndef.HEADER_SIZE
-			self.cmacOffset = offset2 + Ndef.HEADER_SIZE
+			self.piccDataOffset = offset1 + Ndef.URL_HEADER_SIZE
+			self.cmacOffset = offset2 + Ndef.URL_HEADER_SIZE
 		}
 		
 		init(baseText: String) {
@@ -194,13 +260,13 @@ public class Ndef {
 			//
 			// So we need to do our calculations within the string's utf8View.
 			
-			let baseTextLength = fullText.utf8.count
+			let baseTextLength = baseText.utf8.count
 			let offset1 = baseTextLength + 11
 			let offset2 = baseTextLength + 49
 			
 			self.value = Either.Right(fullText)
-			self.piccDataOffset = offset1 + Ndef.HEADER_SIZE
-			self.cmacOffset = offset2 + Ndef.HEADER_SIZE
+			self.piccDataOffset = offset1 + Ndef.TEXT_HEADER_SIZE
+			self.cmacOffset = offset2 + Ndef.TEXT_HEADER_SIZE
 		}
 		
 	} // </struct Template>
