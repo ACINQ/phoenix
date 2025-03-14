61b3c8ad1e9bf2687ca72933bbc37b3a56b9c978
Dominique Padiou <5765435+dpad85@users.noreply.github.com>
2025-03-14 13:00:35 +0100

    (android) Release v2.5.1 (code 101)

--- phoenix-android/build.gradle.kts
+++ phoenix-android/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 101
-        versionName = gitCommitHash()
+        versionName = "2.5.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
