0458b73bd6730b0deda4a4bde9ea8b0198b89442
Dominique Padiou <5765435+dpad85@users.noreply.github.com>
2025-03-24 15:08:11 +0100

    (android) Register network monitoring in start/stop methods

--- phoenix-shared/src/androidMain/kotlin/fr/acinq/phoenix/managers/NetworkMonitorAndroid.kt
+++ phoenix-shared/src/androidMain/kotlin/fr/acinq/phoenix/managers/NetworkMonitorAndroid.kt
@@ -18,53 +18,44 @@ actual class NetworkMonitor actual constructor(loggerFactory: LoggerFactory, val
     private val _networkState = MutableStateFlow(NetworkState.NotAvailable)
     actual val networkState: StateFlow<NetworkState> = _networkState
 
-    init {
-        launch {
-            val connectivityManager = ctx.applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-            connectivityManager.registerDefaultNetworkCallback(
-                object : ConnectivityManager.NetworkCallback() {
+    private val networkCallback = object : ConnectivityManager.NetworkCallback() {
 
-                    override fun onAvailable(network: Network) {
-                        super.onAvailable(network)
-                        logger.debug { "network is now $network" }
-                        connectivityManager.isDefaultNetworkActive
-                        _networkState.value = NetworkState.Available
-                    }
+        override fun onAvailable(network: Network) {
+            super.onAvailable(network)
+            logger.debug { "network is now $network" }
+            _networkState.value = NetworkState.Available
+        }
 
-                    override fun onBlockedStatusChanged(network: Network, blocked: Boolean) {
-                        super.onBlockedStatusChanged(network, blocked)
-                        logger.debug { "block status change to $blocked for network=$network"}
-                    }
+        override fun onBlockedStatusChanged(network: Network, blocked: Boolean) {
+            super.onBlockedStatusChanged(network, blocked)
+            logger.debug { "block status change to $blocked for network=$network"}
+        }
 
-                    override fun onCapabilitiesChanged(network : Network, networkCapabilities : NetworkCapabilities) {
-                        logger.debug { "default network changed capabilities to $networkCapabilities" }
-                    }
+        override fun onCapabilitiesChanged(network : Network, networkCapabilities : NetworkCapabilities) {
+            logger.debug { "default network changed capabilities to $networkCapabilities" }
+        }
 
-                    override fun onLinkPropertiesChanged(network : Network, linkProperties : LinkProperties) {
-                        logger.debug { "default network changed link properties to $linkProperties" }
-                    }
+        override fun onLinkPropertiesChanged(network : Network, linkProperties : LinkProperties) {
+            logger.debug { "default network changed link properties to $linkProperties" }
+        }
 
-                    override fun onLosing(network: Network, maxMsToLive: Int) {
-                        super.onLosing(network, maxMsToLive)
-                        logger.debug { "losing network in ${maxMsToLive}ms..." }
-                    }
+        override fun onLosing(network: Network, maxMsToLive: Int) {
+            super.onLosing(network, maxMsToLive)
+            logger.debug { "losing network in ${maxMsToLive}ms..." }
+        }
 
-                    override fun onLost(network: Network) {
-                        super.onLost(network)
-                        logger.info { "network has been lost" }
-                        _networkState.value = NetworkState.NotAvailable
-                    }
+        override fun onLost(network: Network) {
+            super.onLost(network)
+            logger.info { "network has been lost" }
+            _networkState.value = NetworkState.NotAvailable
+        }
 
-                    override fun onUnavailable() {
-                        super.onUnavailable()
-                        logger.info { "network is unavailable" }
-                        _networkState.value = NetworkState.NotAvailable
-                    }
-                }
-            )
+        override fun onUnavailable() {
+            super.onUnavailable()
+            logger.info { "network is unavailable" }
+            _networkState.value = NetworkState.NotAvailable
         }
     }
-
 
     actual fun enable() {
     }
@@ -73,8 +64,12 @@ actual class NetworkMonitor actual constructor(loggerFactory: LoggerFactory, val
     }
 
     actual fun start() {
+        val connectivityManager = ctx.applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        connectivityManager.registerDefaultNetworkCallback(networkCallback)
     }
 
     actual fun stop() {
+        val connectivityManager = ctx.applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        connectivityManager.unregisterNetworkCallback(networkCallback)
     }
 }
