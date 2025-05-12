f7325bf0e386d993a17151fd58c7967f89abf0fb
Dominique Padiou <5765435+dpad85@users.noreply.github.com>
2025-05-12 16:53:40 +0200

    (android) Release v2.6.0 (code 104)

--- phoenix-android/build.gradle.kts
+++ phoenix-android/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 104
-        versionName = gitCommitHash()
+        versionName = "2.6.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
