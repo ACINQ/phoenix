d358db8b4ea82e1c52f4096285a3bc647e683d0e
Robbie Hanson <304604+robbiehanson@users.noreply.github.com>
2025-05-20 10:22:31 -0500

    (ios) Bug fixes for Ndef

--- phoenix-ios/phoenix-ios/nfc/tools/Ndef.swift
+++ phoenix-ios/phoenix-ios/nfc/tools/Ndef.swift
@@ -14,7 +14,7 @@ struct NdefHeaderFlags: OptionSet {
 	/// IL (ID Length) is present
 	static let IL = NdefHeaderFlags(rawValue: 0b00001000)
 	
-	
+	/// Type Name Format options:
 	static let TNF_WELL_KNOWN   = NdefHeaderFlags(rawValue: 0b00000001) // 0x01
 	static let TNF_MIME         = NdefHeaderFlags(rawValue: 0b00000010) // 0x02
 	/// Note: don't use this for URLS, use WELLKNOWN instead.
@@ -53,18 +53,6 @@ public class Ndef {
 			0x00, // Placeholder for NLEN
 		]
 		
-		// NDEF Message header:
-		
-		let flags: NdefHeaderFlags = [.MB, .ME, .SR, .TNF_WELL_KNOWN]
-		let type = NdefHeaderType.URL
-		
-		var messageHeader: [UInt8] = [
-			flags.rawValue, // NDEF header flags
-			0x01,           // Type length
-			0x00,           // URL length placeholder
-			type.rawValue   // Well-known type: URL
-		]
-		
 		// Header for: Well-known-type(URL)
 		//
 		// Note: If you have a long URL that doesn't fit, you can change the typeHeader here.
@@ -78,14 +66,49 @@ public class Ndef {
 		let urlData = url.absoluteString.data(using: .utf8) ?? Data()
 		var urlBytes =  urlData.toByteArray()
 		
-		let maxFileSize = 256 // As per NTAG 424 spcification for File #2
-		let maxUrlSize = maxFileSize - (fileHeader.count + messageHeader.count + typeHeader.count)
-		if urlBytes.count > maxUrlSize {
-			urlBytes = Array(urlBytes[0..<maxUrlSize])
+		// NDEF Message header:
+		
+		let messageHeader: [UInt8]
+		
+		let fitsInShortRecord = (typeHeader.count + urlBytes.count) <= 255
+		if fitsInShortRecord {
+			
+			let payloadLength = UInt8(typeHeader.count + urlBytes.count)
+			
+			let flags: NdefHeaderFlags = [.MB, .ME, .SR, .TNF_WELL_KNOWN]
+			let type = NdefHeaderType.URL
+			
+			messageHeader = [
+				flags.rawValue, // NDEF header flags
+				0x01,           // Type length
+				payloadLength,  // Payload length (SR = 1 byte)
+				type.rawValue   // Well-known type: URL
+			]
+			
+		} else {
+			
+			let payloadLengthLE = UInt32(typeHeader.count + urlBytes.count)
+			let payloadLength: [UInt8] = payloadLengthLE.bigEndian.toByteArray()
+			
+			let flags: NdefHeaderFlags = [.MB, .ME, .TNF_WELL_KNOWN]
+			let type = NdefHeaderType.URL
+			
+			messageHeader = [
+				flags.rawValue,   // NDEF header flags
+				0x01,             // Type length
+				payloadLength[0], // Payload length (!SR = 4 bytes)
+				payloadLength[1], // Payload length
+				payloadLength[2], // Payload length
+				payloadLength[3], // Payload length
+				type.rawValue     // Well-known type: URL
+			]
 		}
 		
-		fileHeader[1] = UInt8(messageHeader.count + typeHeader.count + urlBytes.count)
-		messageHeader[2] = UInt8(typeHeader.count + urlBytes.count)
+		let fileLengthLE = UInt16(messageHeader.count + typeHeader.count + urlBytes.count)
+		let fileLength: [UInt8] = fileLengthLE.bigEndian.toByteArray()
+		
+		fileHeader[0] = fileLength[0]
+		fileHeader[1] = fileLength[1]
 		
 		let result: [UInt8] = fileHeader + messageHeader + typeHeader + urlBytes
 		return result
@@ -107,18 +130,6 @@ public class Ndef {
 		var fileHeader: [UInt8] = [
 			0x00, // Placeholder for NLEN
 			0x00  // Placeholder for NLEN
-		]
-		
-		// NDEF Message header:
-		
-		let flags: NdefHeaderFlags = [.MB, .ME, .SR, .TNF_WELL_KNOWN]
-		let type = NdefHeaderType.TEXT
-		
-		var messageHeader: [UInt8] = [
-			flags.rawValue, // NDEF header flags
-			0x01,           // Type length
-			0x00,           // Text length placeholder
-			type.rawValue   // Well-known type: TEXT
 		]
 		
 		// Header for: Well-known-type(TEXT)
@@ -151,117 +162,51 @@ public class Ndef {
 		let textData = text.data(using: .utf8) ?? Data()
 		var textBytes = textData.toByteArray()
 		
-		let maxFileSize = 256 // As per NTAG 424 spcification for File #2
-		let maxTextSize = maxFileSize - (fileHeader.count + messageHeader.count + typeHeader.count)
-		if textBytes.count > maxTextSize {
-			textBytes = Array(textBytes[0..<maxTextSize])
+		// NDEF Message header:
+		
+		let messageHeader: [UInt8]
+		
+		let fitsInShortRecord = (typeHeader.count + textBytes.count) <= 255
+		if fitsInShortRecord {
+			
+			let payloadLength = UInt8(typeHeader.count + textBytes.count)
+			
+			let flags: NdefHeaderFlags = [.MB, .ME, .SR, .TNF_WELL_KNOWN]
+			let type = NdefHeaderType.TEXT
+			
+			messageHeader = [
+				flags.rawValue, // NDEF header flags
+				0x01,           // Type length
+				payloadLength,  // Payload length (SR = 1 byte)
+				type.rawValue   // Well-known type: TEXT
+			]
+			
+		} else {
+			
+			let payloadLengthLE = UInt32(typeHeader.count + textBytes.count)
+			let payloadLength: [UInt8] = payloadLengthLE.bigEndian.toByteArray()
+			
+			let flags: NdefHeaderFlags = [.MB, .ME, .TNF_WELL_KNOWN]
+			let type = NdefHeaderType.URL
+			
+			messageHeader = [
+				flags.rawValue,   // NDEF header flags
+				0x01,             // Type length
+				payloadLength[0], // Payload length (!SR = 4 bytes)
+				payloadLength[1], // Payload length
+				payloadLength[2], // Payload length
+				payloadLength[3], // Payload length
+				type.rawValue     // Well-known type: URL
+			]
 		}
 		
-		fileHeader[1] = UInt8(messageHeader.count + typeHeader.count + textBytes.count)
-		messageHeader[2] = UInt8(typeHeader.count + textBytes.count)
+		let fileLengthLE = UInt16(messageHeader.count + typeHeader.count + textBytes.count)
+		let fileLength: [UInt8] = fileLengthLE.bigEndian.toByteArray()
+		
+		fileHeader[0] = fileLength[0]
+		fileHeader[1] = fileLength[1]
 		
 		let result: [UInt8] = fileHeader + messageHeader + typeHeader + textBytes
 		return result
 	}
-	
-	class func ndefDataForTemplate(_ template: Template) -> [UInt8] {
-		
-		switch template.value {
-		case .Left(let url):
-			return ndefDataForUrl(url)
-		case .Right(let text):
-			return ndefDataForText(text)
-		}
-	}
-	
-	struct Template {
-		let value: Either<URL, String>
-		let piccDataOffset: Int
-		let cmacOffset: Int
-		
-		var valueString: String {
-			switch value {
-			case .Left(let url):
-				return url.absoluteString
-			case .Right(let text):
-				return text
-			}
-		}
-		
-		init?(baseUrl: URL) {
-			
-			guard var comps = URLComponents(url: baseUrl, resolvingAgainstBaseURL: false) else {
-				return nil
-			}
-			
-			var queryItems = comps.queryItems ?? []
-			
-			// The `baseUrl` SHOULD NOT have either `picc_data` or `cmac` parameters.
-			// But just to be safe, we'll remove them if they're present.
-			//
-			queryItems.removeAll(where: { item in
-				let name = item.name.lowercased()
-				return name == "picc_data" || name == "cmac"
-			})
-			
-			// picc_data=(16_bytes_hexadecimal)
-			// cmac=(8_bytes_hexadecimal)
-			
-			queryItems.append(URLQueryItem(name: "picc_data", value: "00000000000000000000000000000000"))
-			queryItems.append(URLQueryItem(name: "cmac",      value: "0000000000000000"))
-			
-			comps.queryItems = queryItems
-			
-			guard let resolvedUrl = comps.url else {
-				return nil
-			}
-			
-			let urlString = resolvedUrl.absoluteString
-			
-			// Ultimately, the URL gets encoded as UTF-8,
-			// and the offsets are used as indexes within this UTF-8 representation.
-			//
-			// So we need to do our calculations within the string's utf8View.
-			
-			let urlUtf8 = urlString.utf8
-			
-			guard let range1 = urlUtf8.ranges(of: "picc_data=".utf8).last else {
-				return nil
-			}
-			let offset1 = urlUtf8.distance(from: urlUtf8.startIndex, to: range1.upperBound)
-			
-			guard let range2 = urlUtf8.ranges(of: "cmac=".utf8).last else {
-				return nil
-			}
-			let offset2 = urlUtf8.distance(from: urlUtf8.startIndex, to: range2.upperBound)
-			
-			self.value = Either.Left(resolvedUrl)
-			self.piccDataOffset = offset1 + Ndef.URL_HEADER_SIZE
-			self.cmacOffset = offset2 + Ndef.URL_HEADER_SIZE
-		}
-		
-		init(baseText: String) {
-			
-			// picc_data=(16_bytes_hexadecimal)
-			// cmac=(8_bytes_hexadecimal)
-			
-			let fullText = "\(baseText)?picc_data=00000000000000000000000000000000&cmac=0000000000000000"
-			//                         +123456789 123456789 123456789 123456789 123456789
-			//                                    ^+11                                  ^+49
-			
-			// Ultimately, the value gets encoded as UTF-8,
-			// and the offsets are used as indexes within this UTF-8 representation.
-			//
-			// So we need to do our calculations within the string's utf8View.
-			
-			let baseTextLength = baseText.utf8.count
-			let offset1 = baseTextLength + 11
-			let offset2 = baseTextLength + 49
-			
-			self.value = Either.Right(fullText)
-			self.piccDataOffset = offset1 + Ndef.TEXT_HEADER_SIZE
-			self.cmacOffset = offset2 + Ndef.TEXT_HEADER_SIZE
-		}
-		
-	} // </struct Template>
 }
